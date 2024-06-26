0742aeb34bae44b25f45a9f6140ec94ccc902f19
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:04:16 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/06. ConditionalStatementsExercise/05. GodzillaVsKong/Program.cs
@@ -0,0 +1,24 @@
+﻿double moneyForMovie = double.Parse(Console.ReadLine());
+int numberOfExtras = int.Parse(Console.ReadLine());
+double priceForClotesForOneExtras = double.Parse(Console.ReadLine());
+
+double sumForDecoor = moneyForMovie * 0.1;
+double sumForClotes = numberOfExtras * priceForClotesForOneExtras;
+double allMoneyForMovie = moneyForMovie - (sumForDecoor + sumForClotes);
+
+if (numberOfExtras > 150)
+{
+    sumForClotes = sumForClotes * 0.9;
+    allMoneyForMovie = moneyForMovie - sumForClotes;
+}
+if  (sumForDecoor + sumForClotes > moneyForMovie)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {(sumForDecoor + sumForClotes) - moneyForMovie:f2} leva more.");
+}
+else
+{
+    allMoneyForMovie = moneyForMovie - (sumForDecoor + sumForClotes);
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {allMoneyForMovie:f2} leva left.");
+}
