c6f7e1c87e97698073003d996813e95a85c4f991
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:04:44 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/09. SimpleConditionalStatementsLab/9. AreaOfFigures/Program.cs
@@ -0,0 +1,21 @@
+﻿string typeOfFigure = Console.ReadLine();
+
+if (typeOfFigure == "square")
+{
+    double sideOfSquare = double.Parse(Console.ReadLine());
+    double area = sideOfSquare * sideOfSquare;
+    Console.WriteLine($"{area:f2}");
+}
+else if (typeOfFigure == "rectangle")
+{
+    double width = double.Parse(Console.ReadLine());
+    double length = double.Parse(Console.ReadLine());
+    double area = width * length;
+    Console.WriteLine($"{area:f2}");
+}
+else if (typeOfFigure == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    double area = Math.PI * radius * radius;
+    Console.WriteLine($"{area:f2}");
+}
