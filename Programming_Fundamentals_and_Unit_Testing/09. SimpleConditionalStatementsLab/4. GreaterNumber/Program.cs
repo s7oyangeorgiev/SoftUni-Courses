2cd5dbe41974cf61de032c154bc95acc97a3f147
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:02:41 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/09. SimpleConditionalStatementsLab/4. GreaterNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo =  int.Parse(Console.ReadLine());
+
+if (numOne > numTwo)
+{
+    Console.WriteLine($"Greater number: {numOne}");
+}
+else
+{
+    Console.WriteLine($"Greater number: {numTwo}");
+}
