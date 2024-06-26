66f978c2dccc7ed4fb857d02ed908f358f81b088
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:02:55 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/06. ConditionalStatementsExercise/02. BonusScore/Program.cs
@@ -0,0 +1,25 @@
+﻿int points = int.Parse(Console.ReadLine());
+double bonusPoints = 0.0;
+
+if (points <= 100)
+{
+    bonusPoints += 5;
+}
+else if (points <= 1000)
+{
+    bonusPoints = points * 0.2;
+}
+else
+{
+    bonusPoints = points * 0.1;
+}
+if (points % 2 == 0)
+{
+    bonusPoints += 1;
+}
+else if (points % 10 == 5)
+{
+    bonusPoints += 2;
+}
+Console.WriteLine(bonusPoints);
+Console.WriteLine(points + bonusPoints);
