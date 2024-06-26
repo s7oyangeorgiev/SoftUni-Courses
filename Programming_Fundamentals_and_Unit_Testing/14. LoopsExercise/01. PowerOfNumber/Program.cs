edc5bed58ce7c6ea7afdf141dbdcd37bbb65bae8
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:20:20 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/01. PowerOfNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int numN = int.Parse(Console.ReadLine());
+int powerNum = int.Parse(Console.ReadLine());
+
+int sum = numN;
+
+for (int i = 2; i <= powerNum; i++)
+{
+    sum = sum * numN;
+}
+
+Console.WriteLine(sum);
