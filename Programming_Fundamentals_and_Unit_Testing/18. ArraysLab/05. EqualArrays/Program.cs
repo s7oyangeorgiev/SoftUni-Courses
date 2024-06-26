90c8949d63005e62c9012060e09d6362b244994b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:20:40 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/18. ArraysLab/05. EqualArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] firstArray = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int[] secondArray = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+bool isIdential = false;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    int firstArrayCurrentNumber = firstArray[i];
+    int secondArrayCurrentNumber = secondArray[i];
+
+    if (firstArrayCurrentNumber == secondArrayCurrentNumber)
+    {
+        isIdential = true;
+        continue;
+    }
+    else
+    {
+        isIdential = false;
+        break;
+    }
+   
+}
+
+if (isIdential)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
