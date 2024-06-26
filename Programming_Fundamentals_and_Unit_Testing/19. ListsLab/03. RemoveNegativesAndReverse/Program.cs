6566dac9c3272d2fb303f001f94a61f86e38c8aa
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:22:51 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/19. ListsLab/03. RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+numbers.RemoveAll(n => n < 0);
+
+if (numbers.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    numbers.Reverse();
+    Console.WriteLine(string.Join(" ", numbers));
+}
