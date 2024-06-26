32f924d5b3d19b04e4da6a37b00c50566dde2c0c
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:22:11 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/08. ConditionalStatementsAdvancedExercise/06. OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+
+double result = 0;
+
+if (operation == "+")
+{
+    result = numOne + numTwo;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{numOne} {operation} {numTwo} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{numOne} + {numTwo} = {result} - odd");
+    }
+}
+else if (operation == "-")
+{
+    result = numOne - numTwo;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{numOne} - {numTwo} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{numOne} - {numTwo} = {result} - odd");
+    }
+}
+else if ( operation == "*")
+{
+    result = numOne * numTwo;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{numOne} * {numTwo} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{numOne} * {numTwo} = {result} - odd");
+    }
+}
+else if ( operation == "/")
+{
+    if (numTwo == 0)
+    {
+        Console.WriteLine($"Cannot divide {numOne} by zero");
+    }
+    else
+    {
+        result = (double)numOne / numTwo;
+        Console.WriteLine($"{numOne} / {numTwo} = {result:f2}");
+    }
+}
+else if( operation == "%")
+{
+    if (numTwo == 0)
+    {
+        Console.WriteLine($"Cannot divide {numOne} by zero");
+    }
+    else
+    {
+        result = (double)numOne % numTwo;
+        Console.WriteLine($"{numOne} % {numTwo} = {result}");
+    }
+}
