befe26897c76dc0b28a85e59cdaecf13db5380c2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:02:29 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/06. ConditionalStatementsExercise/01. SumSeconds/Program.cs
@@ -0,0 +1,18 @@
+﻿int racerOne = int.Parse(Console.ReadLine()); 
+int racerTwo = int.Parse(Console.ReadLine());
+int racerTree = int.Parse(Console.ReadLine());
+
+int allTimeSum = racerOne + racerTwo + racerTree;
+int minutes = allTimeSum / 60;
+int seconds = allTimeSum % 60;
+
+if (seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}");
+}
+
+
