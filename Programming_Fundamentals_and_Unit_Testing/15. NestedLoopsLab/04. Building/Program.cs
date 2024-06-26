f1f9f465bbf71ac7962e68cb39e621983e41f7fd
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:29:23 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/15. NestedLoopsLab/04. Building/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int floorsCount = int.Parse(Console.ReadLine());
+int estatesCount = int.Parse(Console.ReadLine());
+
+for (int floors = floorsCount; floors >= 1; floors--)
+{
+    for (int apartments = 0; apartments < estatesCount; apartments++)
+    {
+        if (floors == floorsCount)
+        {
+            Console.Write($"L{floors}{apartments} ");
+            continue;
+        }
+        else if (floors % 2 == 1)
+        {
+            Console.Write($"A{floors}{apartments} ");
+            continue;
+        }
+        else if (floors % 2 == 0)
+        {
+            Console.Write($"O{floors}{apartments} ");
+            continue;
+        }
+    }
+    Console.WriteLine();
+}
