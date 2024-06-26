b36fac020eea817f47de5dd66d6fb4214f1240ed
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:54:02 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/07. SimpleCalculationsLab/03. Market/Program.cs
@@ -0,0 +1,11 @@
+﻿double tomatoPrice = double.Parse(Console.ReadLine());
+double tomatoQuantity = double.Parse(Console.ReadLine());
+double cucumberPrice =  double.Parse(Console.ReadLine());
+double cucumberQuantity =  double.Parse(Console.ReadLine());
+
+double tomatoCosts = tomatoQuantity * tomatoPrice;
+double cucumberCosts = cucumberQuantity * cucumberPrice;
+
+double tottalCosts = tomatoCosts + cucumberCosts;
+
+Console.WriteLine($"{tottalCosts:f2}");
