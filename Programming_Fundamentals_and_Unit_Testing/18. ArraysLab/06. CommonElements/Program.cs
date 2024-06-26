3734b8fb8d2bda8ad028d7337fcee5766addc047
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:21:08 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/18. ArraysLab/06. CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] firstArray = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int[] secondArray = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i <= firstArray.Length - 1; i++)
+{
+    int currentNumInFirstArray = firstArray[i];
+
+    foreach (int number in secondArray)
+    {
+        int currentNumberInSecondArray = number;
+
+        if ( currentNumInFirstArray == number)
+        {
+            Console.Write(currentNumInFirstArray + " ");
+        }
+    }
+}
+Console.WriteLine();
+
