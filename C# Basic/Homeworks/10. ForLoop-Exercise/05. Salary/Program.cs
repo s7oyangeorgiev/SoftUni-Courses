6ce48035a61a75999e47f7f63ff704e54249173f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:36:15 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/10. ForLoop-Exercise/05. Salary/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int tabsCount = int.Parse(Console.ReadLine());
+double salary = double.Parse(Console.ReadLine());
+
+for (int i = 0; i < tabsCount; i++)
+{
+    string tabsName = Console.ReadLine();
+
+    if (tabsName == "Facebook")
+    {
+        salary -= 150;
+        
+    }
+    else if (tabsName == "Instagram")
+    {
+        salary -= 100;
+    }
+    else if (tabsName == "Reddit")
+    {
+        salary -= 50;
+    }
+    if (salary <= 0)
+    {
+        break;
+    }
+}
+if (salary > 0)
+{
+    Console.WriteLine($"{salary}");
+}
+else
+{
+    Console.WriteLine("You have lost your salary.");
+}
