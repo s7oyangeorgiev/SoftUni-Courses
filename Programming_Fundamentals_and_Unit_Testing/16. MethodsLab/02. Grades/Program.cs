2c0726080ef1746746866f6e22be8163bd1dde7e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:21:17 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/02. Grades/Program.cs
@@ -0,0 +1,14 @@
+﻿double grade = double.Parse(Console.ReadLine());
+ReceivesGrade(grade);
+
+static void ReceivesGrade(double grade)
+{
+    switch (grade)
+    {
+        case >= 2.00 and <= 2.99: Console.WriteLine("Fail"); break;
+        case >= 3.00 and <= 3.49: Console.WriteLine("Average"); break;
+        case >= 3.50 and <= 4.49: Console.WriteLine("Good"); break;
+        case >= 4.50 and <= 5.49: Console.WriteLine("Very good"); break;
+        case >= 5.50 and <= 6.00: Console.WriteLine("Excellent"); break;
+    }
+}
