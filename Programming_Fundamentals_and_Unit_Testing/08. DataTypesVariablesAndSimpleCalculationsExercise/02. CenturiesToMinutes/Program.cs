72d5a4a4e3245e9c30758bcea7d7ffaac4f2eef6
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:57:24 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/08. DataTypesVariablesAndSimpleCalculationsExercise/02. CenturiesToMinutes/Program.cs
@@ -0,0 +1,9 @@
+﻿int centuries = int.Parse(Console.ReadLine());
+
+int centuriesToYears = centuries * 100;
+double yearsToDays = Math.Floor(centuriesToYears * 365.2422);
+double daysToHours = yearsToDays * 24;
+double hoursToMinutes = daysToHours * 60;
+
+Console.WriteLine($"{centuries} centuries = {centuriesToYears} years = " +
+    $"{yearsToDays} days = {daysToHours} hours = {hoursToMinutes} minutes");
