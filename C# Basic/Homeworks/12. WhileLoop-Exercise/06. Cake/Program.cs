3a98c121bcf404ed40e151432ce1abeafa3f63ae
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:45:56 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/12. WhileLoop-Exercise/06. Cake/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+int piecesCount = width * height;
+int onePiece = 1;
+
+while (piecesCount > 0)
+{
+    string command = Console.ReadLine();
+
+    if (command == "STOP")
+    {
+        break;
+    }
+    else
+    {
+        int pieces = int.Parse(command);
+        piecesCount -= pieces;
+    }
+}
+if (piecesCount > 0)
+{
+    Console.WriteLine($"{piecesCount} pieces are left.");
+}
+else
+{
+    Console.WriteLine($"No more cake left! You need {Math.Abs(piecesCount)} pieces more.");
+}
