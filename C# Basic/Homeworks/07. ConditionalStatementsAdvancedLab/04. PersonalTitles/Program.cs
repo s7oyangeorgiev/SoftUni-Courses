b44cf8730c3047af3258afa2e52e98db6c808221
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:14:39 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/07. ConditionalStatementsAdvancedLab/04. PersonalTitles/Program.cs
@@ -0,0 +1,26 @@
+﻿double age = double.Parse(Console.ReadLine());
+string gender = Console.ReadLine();
+
+if (gender == "m")
+{
+    if (age < 16)
+    {
+        Console.WriteLine("Master");
+    }
+    else 
+    { 
+        Console.WriteLine("Mr."); 
+    }
+    
+}
+else if (gender == "f")
+{
+    if (age < 16)
+    {
+        Console.WriteLine("Miss");
+    }
+    else
+    {
+        Console.WriteLine("Ms.");
+    }
+}
