f12148b2451e885d6d0b8443a177cfa0da2f3bd3
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:24:52 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/09. Orders/Program.cs
@@ -0,0 +1,17 @@
+﻿string product = Console.ReadLine();
+double quantity = double.Parse(Console.ReadLine());
+double result = TotalPrice(product, quantity);
+Console.WriteLine($"{result:f2}");
+
+static double TotalPrice(string product, double quantity)
+{
+    double totalPrice = 0;
+    switch (product)
+    {
+        case "coffee": totalPrice = quantity * 1.50; break;
+        case "water": totalPrice = quantity * 1.00; break;
+        case "coke": totalPrice = quantity * 1.40; break;
+        case "snacks": totalPrice = quantity * 2.00; break;
+    }
+    return totalPrice;
+}
