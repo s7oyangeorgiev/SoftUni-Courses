764080d33947a68bcc8fb783811c07015195edb6
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:30:16 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/15. NestedLoopsLab/06. TravelSavings/Program.cs
@@ -0,0 +1,25 @@
+﻿string destination = "";
+
+while (destination != "End")
+{
+    destination = Console.ReadLine();
+
+    if (destination == "End")
+    {
+        break;
+    }
+
+    int budgetNeeded = int.Parse(Console.ReadLine());
+    int collectedMoney = 0;
+
+    while (budgetNeeded > 0)
+    {
+        int currendBudget = int.Parse(Console.ReadLine());
+        budgetNeeded -= currendBudget;
+        collectedMoney += currendBudget;
+        Console.WriteLine($"Collected: {collectedMoney:f2}");
+
+    }
+    Console.WriteLine($"Going to {destination}!");
+    collectedMoney = 0;
+}
