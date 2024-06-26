ffb3ecdc7191d37941711ef80a8804d1c46a131e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:05:17 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/09. SimpleConditionalStatementsLab/10. ValidTriangle/Program.cs
@@ -0,0 +1,12 @@
+﻿int sideA = int.Parse(Console.ReadLine());
+int sideB = int.Parse(Console.ReadLine());
+int sideC = int.Parse(Console.ReadLine());
+
+if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
+{
+    Console.WriteLine("Valid Triangle");
+}
+else
+{
+    Console.WriteLine("Invalid Triangle");
+}
