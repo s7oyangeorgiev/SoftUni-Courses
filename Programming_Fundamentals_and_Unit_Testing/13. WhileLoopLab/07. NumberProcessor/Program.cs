5ee8e32eb5a53a2959f5951b1aa10950cd31ce76
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:19:03 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/13. WhileLoopLab/07. NumberProcessor/Program.cs
@@ -0,0 +1,25 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int lastNum = num;
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if ( command == "Inc")
+    {
+        lastNum += 1;
+        continue;
+    }
+    else if ( command == "Dec")
+    {
+        lastNum -= 1;
+        continue;
+    }
+    else if (command == "End")
+    {
+        Console.WriteLine(lastNum);
+        break;
+    }
+}
+
