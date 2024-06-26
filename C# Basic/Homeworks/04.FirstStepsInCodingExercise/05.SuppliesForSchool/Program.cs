c854711433c06d90e3a6ac505a669c82cb34b34b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:54:24 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/04.FirstStepsInCodingExercise/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,14 @@
+﻿int packsOfPens = int.Parse(Console.ReadLine());
+int packsOfMarkers = int.Parse(Console.ReadLine());
+int chalkBoardCleanerInLitters = int.Parse(Console.ReadLine());
+int discoundPercent = int.Parse(Console.ReadLine()); //100
+
+double priceOfPacksOfPens = packsOfPens * 5.80;
+double priceOfPacksOfMarkers = packsOfMarkers * 7.20;
+double priceOfChalkBoardCleanerInLitters = chalkBoardCleanerInLitters * 1.20;
+
+double priceOfAllMaterials = priceOfPacksOfPens + priceOfPacksOfMarkers + priceOfChalkBoardCleanerInLitters;
+double discount = priceOfAllMaterials * discoundPercent / 100;
+
+Console.WriteLine(priceOfAllMaterials - discount);
+
