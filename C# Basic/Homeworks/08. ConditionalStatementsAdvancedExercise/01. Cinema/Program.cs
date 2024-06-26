60308f9bdbee0f6f8ce3c920e4d7a0aa33087e93
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:19:58 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/08. ConditionalStatementsAdvancedExercise/01. Cinema/Program.cs
@@ -0,0 +1,15 @@
+﻿string project = Console.ReadLine();
+int lines = int.Parse(Console.ReadLine());
+int colonies = int.Parse(Console.ReadLine());
+
+double priceproject = 0;
+
+switch (project)
+{
+    case "Premiere": priceproject = 12.00; break;
+    case "Normal": priceproject = 7.50; break;
+    case "Discount": priceproject = 5.00; break;
+    default: Console.WriteLine("unknown project"); return;
+}
+double totalPrice = lines * colonies * priceproject;
+Console.WriteLine($"{totalPrice:f2} leva");
