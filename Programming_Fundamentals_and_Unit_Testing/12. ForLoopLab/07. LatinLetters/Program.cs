cd83d0f03068c3569930774d529466cf04efa2b0
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:59:41 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/12. ForLoopLab/07. LatinLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿char firstLetter = char.Parse(Console.ReadLine());
+char secondLetter = char.Parse(Console.ReadLine());
+
+string result = "";
+
+for (char i = firstLetter; i <= secondLetter; i++)
+{
+    result += i;
+    result += " ";
+}
+
+Console.WriteLine(result);
