2b1a2f30576b6cb5c5050e04a1d2195998d6c312
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:28:07 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/07. ZigZagArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+int[] array1 = new int[N];
+int[] array2 = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    string[] input = Console.ReadLine().Split(" ");
+    int num1 = int.Parse(input[0]);
+    int num2 = int.Parse(input[1]);
+
+    if (i % 2 == 0)
+    {
+        array1[i] = num1;
+        array2[i] = num2;
+    }
+    else
+    {
+        array1[i] = num2;
+        array2[i] = num1;
+    }
+}
+Console.WriteLine(string.Join(" ", array1));
+Console.WriteLine(string.Join(" ", array2));
