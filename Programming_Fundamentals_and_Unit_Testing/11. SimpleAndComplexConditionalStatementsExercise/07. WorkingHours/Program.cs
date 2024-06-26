6921953b49b1965e4bdca7008482dbbe67b013c7
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:53:02 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/11. SimpleAndComplexConditionalStatementsExercise/07. WorkingHours/Program.cs
@@ -0,0 +1,19 @@
+﻿int hourOfTheDay = int.Parse(Console.ReadLine());
+string dayOfTheWeek = Console.ReadLine();
+
+if ( dayOfTheWeek == "Monday" || dayOfTheWeek == "Tuesday" || dayOfTheWeek == "Wednesday" ||
+    dayOfTheWeek == "Thursday" || dayOfTheWeek == "Friday" || dayOfTheWeek == "Saturday")
+{
+    if ( hourOfTheDay >= 10 && hourOfTheDay <= 18)
+    {
+        Console.WriteLine("open");
+    }
+    else
+    {
+        Console.WriteLine("closed");
+    }
+}
+else if ( dayOfTheWeek == "Sunday")
+{
+    Console.WriteLine("closed");
+}
