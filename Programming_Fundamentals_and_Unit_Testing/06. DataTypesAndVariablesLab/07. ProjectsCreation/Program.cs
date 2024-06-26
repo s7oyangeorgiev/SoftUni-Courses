7cd8d9234bff36322f331da4d232afd28e31c924
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:52:04 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/06. DataTypesAndVariablesLab/07. ProjectsCreation/Program.cs
@@ -0,0 +1,7 @@
+﻿string architectureName = Console.ReadLine();
+int countOfProjects = int.Parse(Console.ReadLine());
+
+double projectsHoursNeeded = countOfProjects * 3;
+
+Console.WriteLine($"The architect {architectureName} will need {projectsHoursNeeded} hours " +
+    $"to complete {countOfProjects} project/s.");
