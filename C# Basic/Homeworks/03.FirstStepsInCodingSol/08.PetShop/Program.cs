50ac7d8a1e1d244eb4ef22248301a836161061ff
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:49:30 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/03.FirstStepsInCodingSol/08.PetShop/Program.cs
@@ -0,0 +1,8 @@
+﻿int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+double dogFoodTottal = dogFood * 2.50;
+double catFoodTottal = catFood * 4;
+double tottalAmount = catFoodTottal + dogFoodTottal;
+
+Console.WriteLine($"{tottalAmount} lv.");
+
