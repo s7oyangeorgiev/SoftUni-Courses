ac775448f5f6a6647daf1582d894694cf9732905
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:51:37 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/14. NestedLoops-Exercise/01. NumberPyramid/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int num = int.Parse(Console.ReadLine());
+int startingFrom = 1;
+bool isBigger = false;
+
+for (int rows = 1; rows <= num; rows++)
+{
+    for (int cols = 1; cols <= rows; cols++)
+    {
+        if (startingFrom > num)
+        {
+            isBigger = true;
+            break;
+        }
+        Console.Write(startingFrom + " ");
+        startingFrom++;
+    }
+    if (isBigger)
+    {
+        break;
+    }
+    Console.WriteLine();
+}
