9b24871c61c4cf97c4f7485cc146c3d5b72d124f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:18:49 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/18. ArraysLab/01. DayOfWeek/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] daysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+int days = int.Parse(Console.ReadLine());
+
+if (days < 1 || days > 7)
+{
+    Console.WriteLine("Invalid day!");
+    return;
+}
+Console.WriteLine(daysOfTheWeek[days-1]);
