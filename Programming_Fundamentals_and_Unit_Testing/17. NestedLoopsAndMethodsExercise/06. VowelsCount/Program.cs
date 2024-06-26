8a4a1cb5e3daec694219ff80ec68b0a5132b734d
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:16:07 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/06. VowelsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿string text = Console.ReadLine();
+VowelsLetterCount(text);
+static void VowelsLetterCount(string text)
+{
+    int vowels = 0;
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        char currentLetter = text[i];
+        
+
+        switch (currentLetter)
+        {
+            case 'a': vowels++; break;
+            case 'e': vowels++; break;
+            case 'o': vowels++; break;
+            case 'i': vowels++; break;
+            case 'u': vowels++; break;
+            case 'A': vowels++; break;
+            case 'E': vowels++; break;
+            case 'O': vowels++; break;
+            case 'I': vowels++; break;
+            case 'U': vowels++; break;
+        }
+    }
+    Console.Write(vowels);
+}
