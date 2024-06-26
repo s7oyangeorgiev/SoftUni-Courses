a44f7bc362a24163fafd51a52ccfb6ba16886fa4
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:09:45 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/10. ComplexConditionalStatementsLab/07. VowelOrConsonant/Program.cs
@@ -0,0 +1,11 @@
+﻿char letter = char.Parse(Console.ReadLine());
+
+if ( letter == 'a' || letter == 'A' || letter == 'E' || letter == 'e' || letter == 'I' || letter == 'i' ||
+    letter == 'O' || letter == 'o' || letter == 'U' || letter == 'u')
+{
+    Console.WriteLine("Vowel");
+}
+else
+{
+    Console.WriteLine("Consonant");
+}
