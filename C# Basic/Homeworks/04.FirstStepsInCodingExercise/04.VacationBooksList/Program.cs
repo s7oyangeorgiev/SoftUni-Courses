86f592ecc04fc582a3236c78e1a9f0b8e414645e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:53:53 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/04.FirstStepsInCodingExercise/04.VacationBooksList/Program.cs
@@ -0,0 +1,8 @@
+﻿int pages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int tottalTimePerBook = pages / pagesPerHour;
+int neededHoursPerDay = tottalTimePerBook / days;
+
+Console.WriteLine(neededHoursPerDay);
