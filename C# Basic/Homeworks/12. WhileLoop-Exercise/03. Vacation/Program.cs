e528a46c1a1f377fd13b781a4f9a77a35e18ba99
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:44:37 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/12. WhileLoop-Exercise/03. Vacation/Program.cs
@@ -0,0 +1,40 @@
+﻿
+double moneyNeeded = double.Parse(Console.ReadLine());
+double moneyOnHand = double.Parse(Console.ReadLine());
+
+int spendCounter = 0;
+int daysPast = 0;
+
+while (true)
+{
+    string action = Console.ReadLine();
+    double sum = double.Parse(Console.ReadLine());
+    daysPast++;
+
+    if (action == "spend" && sum > moneyOnHand)
+    {
+        moneyOnHand = 0;
+    }
+    else if (action == "spend")
+    {
+        spendCounter++;
+        moneyOnHand -= sum;
+    }
+    else
+    {
+        spendCounter = 0;
+        moneyOnHand += sum;
+    }
+    
+    if (spendCounter == 5)
+    {
+        Console.WriteLine("You can't save the money.");
+        Console.WriteLine($"{daysPast}");
+        break;
+    }
+    else if (moneyOnHand >= moneyNeeded)
+    {
+        Console.WriteLine($"You saved the money for {daysPast} days.");
+        break;
+    }
+}
