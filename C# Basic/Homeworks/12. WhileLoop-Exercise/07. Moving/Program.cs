00ac41ae56cbe06654f4f00c310d8b8dd80d51c0
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:46:24 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/12. WhileLoop-Exercise/07. Moving/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int width = int.Parse(Console.ReadLine());
+int lenght = int.Parse(Console.ReadLine());
+int hight = int.Parse(Console.ReadLine());
+
+int volume = width * lenght * hight;
+int oneBox = 1;
+
+while (volume > 0)
+{
+    string command = Console.ReadLine();
+
+    if (command == "Done")
+    {
+        break;
+    }
+    else
+    {
+        int boxesCount = int.Parse(command);
+        volume -= boxesCount;
+    }
+}
+if (volume > 0)
+{
+    Console.WriteLine($"{volume} Cubic meters left.");
+}
+else
+{
+    Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+}
