daceccc1328c093d60f7bc8a881ed8545d53b9e9
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:20:48 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/01. SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = int.Parse(Console.ReadLine());
+PrintSign(num);
+
+static void PrintSign(int num)
+{
+    if (num > 0)
+    {
+        Console.WriteLine($"The number {num} is positive.");
+    }
+    else if (num < 0)
+    {
+        Console.WriteLine($"The number {num} is negative.");
+    }
+    else
+    {
+        Console.WriteLine($"The number {num} is zero.");
+    }
+}
