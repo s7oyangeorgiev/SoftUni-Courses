3fd5cbeed2987673cba5c2481faf2807d3965665
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:31:15 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/15. NestedLoopsLab/08. PrimeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿int startOfTheRange = int.Parse(Console.ReadLine());
+int endOfTheRange = int.Parse(Console.ReadLine());
+
+for  (int number = startOfTheRange; number <= endOfTheRange; number++)
+{
+    bool isPrime = true;
+
+    for (int i = 2; i < number; i++)
+    {
+        if ( number % i == 0)
+        {
+            isPrime = false;
+        }
+    }
+    if (isPrime)
+    {
+        Console.Write($"{number} ");
+    }
+}
+
