1f930480dd4248a95e4c85f76a8ce927fd87cbe3
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:24:03 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/08. ConditionalStatementsAdvancedExercise/10. MultiplyBy2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+for (int i = 0;; i++)
+{
+    double numbers = double.Parse(Console.ReadLine());
+    
+    if (numbers >= 0)
+    {
+        
+        Console.WriteLine($"Result: {numbers * 2:f2}");
+    }
+    else
+    {
+        Console.WriteLine("Negative number!");
+        break;
+    }
+}
