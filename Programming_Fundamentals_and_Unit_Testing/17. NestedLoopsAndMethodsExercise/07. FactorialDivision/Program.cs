ee5bdb62b1294b9e3433636dcb9f1ebbe9ff6cbd
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:16:37 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/07. FactorialDivision/Program.cs
@@ -0,0 +1,33 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+
+int totalDevideFactorialResult = TotalFactorialsDevide(numOne,numTwo);
+Console.WriteLine(totalDevideFactorialResult);
+
+
+
+
+static int TotalFactorialsDevide(int numOne, int numTwo)
+{
+    int result = FirstFactorial(numOne) / SecondFactorial(numTwo);
+    return result;
+}
+static int FirstFactorial(int numOne)
+{
+    int result = 1;
+    for (int i = numOne; i >= 1; i--)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+static int SecondFactorial(int numTwo)
+{
+    int result = 1;
+    for (int i = numTwo; i >= 1; i--)
+    {
+        result *= i;
+    }
+    return result;
+}
