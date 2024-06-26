6fcd07a1ac72c5d040ec9905d86b9bc6d83d65cf
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:23:07 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/06. MathPower/Program.cs
@@ -0,0 +1,10 @@
+﻿int baseNumber = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+int resut = BaseRaisedToPower(baseNumber, power);
+Console.WriteLine(resut);
+
+static int BaseRaisedToPower(int baseNumber, int power)
+{
+    int result = (int)Math.Pow(baseNumber, power);
+    return result;
+}
