c1e6fe05ad785d80396e466d7f06bfe1e5aa5cd2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:44:12 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/12. WhileLoop-Exercise/02. ExamPreparation/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int badGrades = int.Parse(Console.ReadLine());
+
+int bagGradesCounter = 0;
+double avarageScore = 0;
+int taskComplieted = 0;
+string lastTask = "";
+
+while (true)
+{
+    string nameOfTask = Console.ReadLine();
+
+    if (nameOfTask == "Enough")
+    {
+        Console.WriteLine($"Average score: {avarageScore / taskComplieted:f2}");
+        Console.WriteLine($"Number of problems: {taskComplieted}");
+        Console.WriteLine($"Last problem: {lastTask}");
+        break;
+    }
+    else
+    {
+        taskComplieted++;
+    }
+
+    int taskAssessment = int.Parse(Console.ReadLine());
+        
+    if (taskAssessment <= 4)
+    {
+        bagGradesCounter++;
+        avarageScore += taskAssessment;
+    }
+    else
+    {
+        avarageScore += taskAssessment;
+    }
+    if (bagGradesCounter >= badGrades)
+    {
+        Console.WriteLine($"You need a break, {bagGradesCounter} poor grades.");
+        break;
+    }
+    lastTask = nameOfTask;
+
+}
