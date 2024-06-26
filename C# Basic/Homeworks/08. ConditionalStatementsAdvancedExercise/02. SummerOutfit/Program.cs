65ab90ddac688d2c82be73a71b65b1e5235c772b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:20:23 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/08. ConditionalStatementsAdvancedExercise/02. SummerOutfit/Program.cs
@@ -0,0 +1,49 @@
+﻿int degrees = int.Parse(Console.ReadLine());
+string time = Console.ReadLine();
+
+string outfit = "";
+string shoes = "";
+
+if (time == "Morning")
+{
+    if (degrees >= 10 && degrees <= 18)
+    {
+        outfit = "Sweatshirt";
+        shoes = "Sneakers";
+    }
+    else if (degrees > 18 && degrees <= 24)
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (degrees >= 25)
+    {
+        outfit = "T-Shirt";
+        shoes = "Sandals";
+    }
+}
+else if (time == "Afternoon")
+{
+    if (degrees >= 10 && degrees <= 18)
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (degrees > 18 && degrees <= 24)
+    {
+        outfit = "T-Shirt";
+        shoes = "Sandals";
+    }
+    else if (degrees >= 25)
+    {
+        outfit = "Swim Suit";
+        shoes = "Barefoot";
+    }
+}
+else if (time == "Evening")
+{
+    outfit = "Shirt";
+    shoes = "Moccasins";
+}
+
+Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
