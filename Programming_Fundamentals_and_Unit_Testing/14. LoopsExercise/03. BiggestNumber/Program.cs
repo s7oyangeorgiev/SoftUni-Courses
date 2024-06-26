03f2efe9289b25e5f06df7874c0145a0185e8d77
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:21:19 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/03. BiggestNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿int numCount = int.Parse(Console.ReadLine());
+
+int biggestNum = int.MinValue;
+
+for (int i = 1; i <= numCount; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > biggestNum)
+    {
+        biggestNum = num;
+    }
+
+}
+
+Console.WriteLine(biggestNum);
