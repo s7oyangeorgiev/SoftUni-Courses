cdab5028f3fde8105b4322fbb9fd2928d5e66cb4
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:23:18 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/19. ListsLab/04. ListManipulationBasics/Program.cs
@@ -0,0 +1,45 @@
+﻿    List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+    while (true)
+    {
+        string command = Console.ReadLine();
+
+        if (command == "end")
+        {
+            break;
+        }
+
+        string[] parts = command.Split(" ");
+
+        switch (parts[0])
+        {
+            case "Add":
+                int numberToAdd = int.Parse(parts[1]);
+                numbers.Add(numberToAdd);
+                break;
+
+            case "Remove":
+                int numberToRemove = int.Parse(parts[1]);
+                numbers.Remove(numberToRemove);
+                break;
+
+            case "RemoveAt":
+                int indexToRemoveAt = int.Parse(parts[1]);
+                numbers.RemoveAt(indexToRemoveAt);
+                break;
+
+            case "Insert":
+                int numberToInsert = int.Parse(parts[1]);
+                int indexToInsertAt = int.Parse(parts[2]);
+                numbers.Insert(indexToInsertAt, numberToInsert);
+                break;
+
+            default:
+                Console.WriteLine("Invalid command");
+                break;
+        }
+    }
+    Console.WriteLine(string.Join(" ", numbers));
