9303a1c4f22d37f2d03a7bda5844521d2ad15b32
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:15:54 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/07. ConditionalStatementsAdvancedLab/07. WorkingHours/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+if (number >= 10 && number <= 18)
+{
+    switch (day)
+    {
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+        case "Saturday":
+            Console.WriteLine("open");
+            break;
+        default: Console.WriteLine("closed"); break;
+    }
+}
+else
+{
+    Console.WriteLine("closed");
+}
