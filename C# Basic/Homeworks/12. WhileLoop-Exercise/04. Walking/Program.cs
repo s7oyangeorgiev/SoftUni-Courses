475393e8b04bac1b0d1f0683c8d4265ecc556569
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:45:05 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/12. WhileLoop-Exercise/04. Walking/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int stepsPerDayToDo = 10_000;
+int tottalStepsPerDay = 0;
+
+while (true)
+{
+    string currentSteps = Console.ReadLine();
+
+    if (currentSteps == "Going home")
+    {
+        int stepsToHome = int.Parse(Console.ReadLine());
+        tottalStepsPerDay += stepsToHome;
+        break;
+    }
+    else
+    {
+        int steps = int.Parse(currentSteps);
+        tottalStepsPerDay += steps;
+
+        if (tottalStepsPerDay >= stepsPerDayToDo)
+        {
+            break;
+        }
+    }
+}
+if (tottalStepsPerDay >= stepsPerDayToDo)
+{
+    Console.WriteLine("Goal reached! Good job!");
+    Console.WriteLine($"{tottalStepsPerDay - stepsPerDayToDo} steps over the goal!");
+}
+else
+{
+    Console.WriteLine($"{stepsPerDayToDo - tottalStepsPerDay} more steps to reach goal.");
+}
