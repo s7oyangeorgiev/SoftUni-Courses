6e1a9d0621e2e052d13458141023d341c5583a8c
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:19:45 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/18. ArraysLab/03. ReverseAnArray/Program.cs
@@ -0,0 +1,8 @@
+﻿int numQuantity = int.Parse(Console.ReadLine());
+int[] numbers = new int[numQuantity];
+
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(string.Join(" ", numbers));
