d931e1bb4552f4c2d810e84f2ac18c0ec94426d6
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:39:35 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/11. WhileLoop-Lab/02. Password/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string userName = Console.ReadLine();
+string password = Console.ReadLine();
+
+while (true)
+{
+    string currentPassword = Console.ReadLine();
+    
+    if (currentPassword == password)
+    {
+        Console.WriteLine($"Welcome {userName}!");
+        break;
+    }
+}
