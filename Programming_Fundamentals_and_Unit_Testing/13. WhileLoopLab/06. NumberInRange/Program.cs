f665add5d4768c25b27f5156e9206f6d95ac2afd
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:18:33 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/13. WhileLoopLab/06. NumberInRange/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (num >= 1 && num <= 100)
+    {
+        Console.WriteLine(num);
+        break;
+    }
+    else
+    {
+        num = int.Parse(Console.ReadLine());
+    }
+}
