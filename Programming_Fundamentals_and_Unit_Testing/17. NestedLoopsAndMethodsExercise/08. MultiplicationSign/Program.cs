0a4bd81a75569d0f4d618117437374a518732af7
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:17:08 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/08. MultiplicationSign/Program.cs
@@ -0,0 +1,25 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numWto = int.Parse(Console.ReadLine());
+int numThree = int.Parse(Console.ReadLine());
+
+string result = "";
+
+if (numOne == 0 || numWto == 0 || numThree == 0)
+{
+    result = "zero";
+}
+else if ((numOne < 0 && numWto < 0 && numThree > 0) || (numOne < 0 && numThree < 0 && numWto > 0) || 
+    (numWto < 0 && numThree < 0 && numOne >0))
+{
+    result = "positive";
+}
+else if (numOne < 0 || numWto < 0 || numThree < 0)
+{
+    result = "negative";
+}
+else
+{
+    result = "positive";
+}
+
+Console.WriteLine(result);
