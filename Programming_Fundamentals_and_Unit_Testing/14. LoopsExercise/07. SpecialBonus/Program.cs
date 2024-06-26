ad3ee0fbf1a653a18337d4daf3762f9694da8f6c
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:23:09 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/07. SpecialBonus/Program.cs
@@ -0,0 +1,17 @@
+﻿double stopNum = int.Parse(Console.ReadLine());
+
+double priviousNum = 0;
+
+while (true)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num == stopNum)
+    {
+        break;
+    }
+
+    priviousNum = num * 1.20;
+}
+
+Console.WriteLine(priviousNum);
