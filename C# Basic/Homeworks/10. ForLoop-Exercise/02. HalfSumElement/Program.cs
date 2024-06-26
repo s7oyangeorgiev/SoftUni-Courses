db08368ea3a113c728e61899c4875e560de971d8
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:31:56 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/10. ForLoop-Exercise/02. HalfSumElement/Program.cs
@@ -0,0 +1,26 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int max = int.MinValue;
+
+for (int i = 1; i <= num; i++)
+{
+    int numbers = int.Parse(Console.ReadLine());
+
+    if (numbers > max)
+    {
+        max = numbers;
+    }
+    sum += numbers;
+}
+if (max == sum - max)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {max}");
+}
+else
+{
+    int diff = max - (sum - max);
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {Math.Abs(diff)}");
+}
