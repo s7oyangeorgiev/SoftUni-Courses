920d6226b78728fb6374340dd494558b3f613a4b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:58:58 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/05.ConditionalStatementsLab/04.PasswordGuess/Program.cs
@@ -0,0 +1,11 @@
+﻿string password = Console.ReadLine();
+string myPassword = "s3cr3t!P@ssw0rd";
+
+if (password == myPassword)
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
