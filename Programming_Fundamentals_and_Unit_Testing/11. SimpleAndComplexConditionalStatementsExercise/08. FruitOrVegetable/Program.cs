298f7c0090ad1a924019e6251fd465a2423390a9
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:53:33 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/11. SimpleAndComplexConditionalStatementsExercise/08. FruitOrVegetable/Program.cs
@@ -0,0 +1,14 @@
+﻿string product = Console.ReadLine();
+
+switch (product)
+{
+    case "banana": Console.WriteLine("fruit"); break;
+    case "apple": Console.WriteLine("fruit"); break;
+    case "kiwi": Console.WriteLine("fruit"); break;
+    case "cherry": Console.WriteLine("fruit"); break;
+    case "lemon": Console.WriteLine("fruit"); break;
+    case "cucumber": Console.WriteLine("vegetable"); break;
+    case "pepper": Console.WriteLine("vegetable"); break;
+    case "carrot": Console.WriteLine("vegetable"); break;
+    default: Console.WriteLine("unknown"); break;
+}
