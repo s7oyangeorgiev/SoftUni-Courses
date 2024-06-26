0634a517653087da3e39deddab87ed8af679a562
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:55:23 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/04.FirstStepsInCodingExercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,19 @@
+﻿int chikenMenus = int.Parse(Console.ReadLine());
+int fishMenus = int.Parse(Console.ReadLine());
+int veggieMenus = int.Parse(Console.ReadLine());
+
+double chikenPrice = 10.35;
+double fishPrice = 12.40;
+double veggiePrice = 8.15;
+double deliveryPrice = 2.50;
+
+double chikenMenuPrice = chikenMenus * chikenPrice;
+double fishMenuPrice = fishMenus * fishPrice;
+double veggieMenuPrice = veggieMenus * veggiePrice;
+double tottalMenusPrice = chikenMenuPrice + fishMenuPrice + veggieMenuPrice;
+double desertPrice = tottalMenusPrice * 0.20;
+
+double tottalMoneyNeeded = tottalMenusPrice + desertPrice + deliveryPrice;
+
+Console.WriteLine(tottalMoneyNeeded); 
+
