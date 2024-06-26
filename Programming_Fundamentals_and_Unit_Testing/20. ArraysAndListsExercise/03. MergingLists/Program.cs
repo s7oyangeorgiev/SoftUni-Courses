7001ca7eb7a2d4e2149f5ec7e4e8d5649a29e178
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:26:08 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/03. MergingLists/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> firstSequence = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+List<int> secondSequence = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+List<int> resultList = new List<int>();
+
+int minLength = Math.Min(firstSequence.Count, secondSequence.Count);
+
+for (int i = 0; i < minLength; i++)
+{
+    resultList.Add(firstSequence[i]);
+    resultList.Add(secondSequence[i]);
+}
+
+if (firstSequence.Count > minLength)
+{
+    resultList.AddRange(firstSequence.GetRange(minLength, firstSequence.Count - minLength));
+}
+else if (secondSequence.Count > minLength)
+{
+    resultList.AddRange(secondSequence.GetRange(minLength, secondSequence.Count - minLength));
+}
+
+Console.WriteLine(string.Join(" ", resultList));
