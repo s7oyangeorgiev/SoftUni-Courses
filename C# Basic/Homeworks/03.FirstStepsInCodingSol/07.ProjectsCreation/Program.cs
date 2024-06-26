acf8a133b9c79794f11d88b0e07926b52f5dcaca
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:49:03 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/03.FirstStepsInCodingSol/07.ProjectsCreation/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = Console.ReadLine();
+int project = int.Parse(Console.ReadLine());
+
+int timeNeeded = project * 3;
+
+Console.WriteLine($"The architect {name} will need {timeNeeded} hours to complete {project} project/s.");
