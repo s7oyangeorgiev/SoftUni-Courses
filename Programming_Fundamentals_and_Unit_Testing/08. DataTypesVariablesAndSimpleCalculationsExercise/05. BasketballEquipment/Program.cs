c1b95bd17c9bbe3f7ecbddacbb87ddd41e797f92
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:58:55 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/08. DataTypesVariablesAndSimpleCalculationsExercise/05. BasketballEquipment/Program.cs
@@ -0,0 +1,11 @@
+﻿int basketballTrainingFee = int.Parse(Console.ReadLine());
+
+int trainingPerYearCosts = basketballTrainingFee;
+double basketballSneakersCosts = trainingPerYearCosts * 0.60;
+double basketballUniformCosts = basketballSneakersCosts * 0.80;
+double basketballBallCosts = basketballUniformCosts * 0.25;
+double basketballAccessoriesCosts = basketballBallCosts * 0.20;
+double totalPrice = trainingPerYearCosts + basketballSneakersCosts + basketballUniformCosts +
+    basketballBallCosts + basketballAccessoriesCosts;
+
+Console.WriteLine(totalPrice);
