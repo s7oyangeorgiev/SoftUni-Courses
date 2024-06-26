b078cce8da72f622280bbefd73aef2b0f104eeee
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:56:16 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/12. ForLoopLab/02. FirstNNumbersSum/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string result = "";
+int totalResult = 0;
+for  (int i = 1; i <= n; i++)
+{
+    result += i;
+    totalResult += i;
+     
+    if (i < n)
+    {
+        result += "+";
+    }
+}
+Console.WriteLine($"{result}={totalResult}");
