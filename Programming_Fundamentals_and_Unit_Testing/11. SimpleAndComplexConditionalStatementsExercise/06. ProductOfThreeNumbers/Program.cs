eacc94042cfd421e881781aab392c1dd4b431965
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:52:30 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/11. SimpleAndComplexConditionalStatementsExercise/06. ProductOfThreeNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+int numThree = int.Parse(Console.ReadLine());
+
+int result = numOne * numTwo * numThree;
+
+if ( result > 0 ) { Console.WriteLine("positive"); }
+else if ( result == 0) { Console.WriteLine("zero"); }
+else { Console.WriteLine("negative"); }
