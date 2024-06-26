f5cb0d43089111fd419a423a11813a669aa010f8
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:53:44 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/07. SimpleCalculationsLab/02. FourOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber  = double.Parse(Console.ReadLine());
+
+double addition = firstNumber + secondNumber;
+double subtraction = firstNumber - secondNumber;
+double multiplication =  secondNumber * firstNumber;
+double division =  firstNumber / secondNumber;
+
+Console.WriteLine($"{firstNumber:f2} + {secondNumber:f2} = {addition:f2}");
+Console.WriteLine($"{firstNumber:f2} - {secondNumber:f2} = {subtraction:f2}");
+Console.WriteLine($"{firstNumber:f2} * {secondNumber:f2} = {multiplication:f2}");
+Console.WriteLine($"{firstNumber:f2} / {secondNumber:f2} = {division:f2}");
