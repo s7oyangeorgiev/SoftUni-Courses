06c7342494b04f93a154c240bfc8bc95d7a49767
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:29:54 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/09. ForLoop-Lab/10. OddEvenSum/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int sumOdd = 0;
+int sumEven = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int numbers = int.Parse(Console.ReadLine());
+    if (i % 2 == 0)
+    {
+        sumEven += numbers;   
+    }
+    else
+    {
+        sumOdd += numbers;
+    } 
+}
+if (sumEven == sumOdd)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {sumEven}");
+}
+else
+{
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {Math.Abs(sumEven - sumOdd)}");
+}
+
+
+
+
+
