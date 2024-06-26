bf395f7de3842552c3835b72853cc7d9e11ecdcf
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:42:20 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/11. WhileLoop-Lab/08. Graduation/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string name = Console.ReadLine();
+
+int level = 1;
+int badGrades = 0;
+double avaregaGrade = 0;
+
+while (level <= 12)
+{
+    double grade = double.Parse(Console.ReadLine());
+
+    if (grade < 4)
+    {
+        badGrades++;
+    }
+    else
+    {
+        level++;
+    }
+        avaregaGrade += grade;
+
+    if (badGrades >= 2)
+    {
+        break;
+    }
+}
+if (badGrades >= 2)
+{
+    Console.WriteLine($"{name} has been excluded at {level} grade");
+}
+else
+{
+    Console.WriteLine($"{name} graduated. Average grade: {avaregaGrade / 12:f2}");
+}
+
