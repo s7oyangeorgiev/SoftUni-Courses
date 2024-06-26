50e490c60bbc396c45d38e6a10c7afb091e31b22
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:22:15 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/05. DivisionTo2,3And4/Program.cs
@@ -0,0 +1,31 @@
+﻿int numCount = int.Parse(Console.ReadLine());
+
+double divisibleBy2 = 0;
+double divisibleBy3 = 0;
+double divisibleBy4 = 0;
+
+for (int i = 1; i <= numCount; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if ( num % 2 == 0)
+    {
+        divisibleBy2 += 1;
+    }
+    if ( num % 3 == 0)
+    {
+        divisibleBy3 += 1;
+    }
+    if ( num % 4 == 0)
+    {
+        divisibleBy4 += 1;
+    }
+}
+
+double divisibleBy2Percent = divisibleBy2 / numCount * 100.00;
+double divisibleBy3Percent = divisibleBy3 / numCount * 100.00;
+double divisibleBy4Percent = divisibleBy4 / numCount * 100.00;
+
+Console.WriteLine($"{divisibleBy2Percent:f2}%");
+Console.WriteLine($"{divisibleBy3Percent:f2}%");
+Console.WriteLine($"{divisibleBy4Percent:f2}%");
