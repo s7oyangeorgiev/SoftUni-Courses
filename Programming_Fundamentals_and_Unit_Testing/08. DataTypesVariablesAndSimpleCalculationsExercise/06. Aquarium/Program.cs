e3af38c7c192894bef02e2cb5074fd23a3f6f043
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:59:25 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/08. DataTypesVariablesAndSimpleCalculationsExercise/06. Aquarium/Program.cs
@@ -0,0 +1,11 @@
+﻿int lengthInCm = int.Parse(Console.ReadLine());
+int widthInCm =  int.Parse(Console.ReadLine());
+int heightInCm =  int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double aquariumVolume = lengthInCm * widthInCm * heightInCm;
+double litersVolume = aquariumVolume / 1000.00;
+double occupiedSpace = percentage / 100;
+double requiredLiters = litersVolume * (1 - occupiedSpace);
+
+Console.WriteLine($"{requiredLiters:f2}");
