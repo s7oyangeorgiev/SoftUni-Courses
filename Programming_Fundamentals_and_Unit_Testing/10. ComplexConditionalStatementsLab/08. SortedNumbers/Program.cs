ba4fc6693ca9265728eeeb92237dc0f5f1952398
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:10:14 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/10. ComplexConditionalStatementsLab/08. SortedNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+int numTree = int.Parse(Console.ReadLine());
+
+if (numOne < numTwo && numTwo < numTree)
+{
+    Console.WriteLine("Ascending");
+}
+else if ( numOne > numTwo && numTwo > numTree)
+{
+    Console.WriteLine("Descending");
+}
+else
+{
+    Console.WriteLine("Not sorted");
+}
