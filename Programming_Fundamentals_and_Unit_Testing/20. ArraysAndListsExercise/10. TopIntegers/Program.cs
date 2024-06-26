ef531d440f7c10cb859189fa7b6083c69a1fe3d5
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:29:34 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/10. TopIntegers/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+List<int> topIntegers = new List<int>();
+
+int maxNumber = int.MinValue;
+
+for (int i = numbers.Count - 1; i >= 0; i--)
+{
+    if (numbers[i] > maxNumber)
+    {
+        topIntegers.Add(numbers[i]);
+        maxNumber = numbers[i];
+    }
+}
+
+topIntegers.Reverse();
+
+Console.WriteLine(string.Join(" ", topIntegers));
