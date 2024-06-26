3bca66d3b86bdb2c6b103dd9ae81ab371b30d53a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:07:28 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/10. ComplexConditionalStatementsLab/02. LargestNumberOutOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿int numOne = int.Parse(Console.ReadLine());
+int numTwo = int.Parse(Console.ReadLine());
+int numThree = int.Parse(Console.ReadLine());
+
+if (numOne > numTwo && numOne > numThree)
+{
+    Console.WriteLine($"{numOne}");
+}
+else if (numTwo > numOne && numTwo > numThree)
+{
+    Console.WriteLine($"{numTwo}");
+}
+else if (numThree > numOne && numThree > numTwo)
+{
+    Console.WriteLine($"{numThree}");
+}
