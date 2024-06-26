46e5db6fb9c529dad4b10fff62aa1e7eb3b30917
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:22:13 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/04. CalculateRectangleArea/Program.cs
@@ -0,0 +1,10 @@
+﻿int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+int area = TriangleArea(width, length);
+Console.WriteLine(area);
+
+static int TriangleArea(int w, int l)
+{
+    int areaResult = w * l;
+    return areaResult;
+}
