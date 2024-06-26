d39ac9205c42dc6d50af32b5675f3bb563cb3c92
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:37:34 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/10. ForLoop-Exercise/08. TennisRanklist/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int tournaments = int.Parse(Console.ReadLine());
+double startingPoints = double.Parse(Console.ReadLine());
+
+double averagePoints = 0;
+double winTour = 0;
+
+for (int i = 0; i < tournaments; i++)
+{
+    string stage = Console.ReadLine();
+
+    if (stage == "W")
+    {
+        averagePoints += 2000;
+        startingPoints += 2000;
+        winTour++;
+    }
+    else if (stage == "F")
+    {
+        averagePoints += 1200;
+        startingPoints += 1200;
+    }
+    else if (stage == "SF")
+    {
+        averagePoints += 720;
+        startingPoints += 720;
+    }
+}
+averagePoints = averagePoints / tournaments;
+
+Console.WriteLine($"Final points: {startingPoints}");
+Console.WriteLine($"Average points: {Math.Floor(averagePoints)}");
+Console.WriteLine($"{(winTour / tournaments) * 100:f2}%");
+
