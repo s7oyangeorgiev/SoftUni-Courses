a755f03197946766a24d5f0eacc10ea055145af9
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:15:35 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/05. HappyNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+for  (int d1 = 1; d1 <= num; d1++)
+{
+    for (int d2 = 0; d2 <= num; d2++)
+    {
+        for (int d3 = 0; d3 <= num; d3++)
+        {
+            for ( int  d4 = 0; d4 <= num; d4++)
+            {
+                if (d1 + d2 == num && d3 + d4 == num && d1 + d2 == d3 + d4)
+                {
+                    Console.Write($"{d1}{d2}{d3}{d4} ");
+                }
+            }
+        }
+    }
+}
+Console.WriteLine();
