cff797705e419724b0b6b775be60ecbf6e01ff20
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:54:26 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/07. SimpleCalculationsLab/04. Tiles/Program.cs
@@ -0,0 +1,11 @@
+﻿double bathroomWidth = double.Parse(Console.ReadLine());
+double bathroomHeight =  double.Parse(Console.ReadLine());
+double tileWidth =  double.Parse(Console.ReadLine());
+double tileHeight = double.Parse(Console.ReadLine());
+
+double bathroomArea = bathroomWidth * bathroomHeight;
+double tottalBathroomArea = bathroomArea * 1.1;
+double tileArea = tileHeight * tileWidth;
+double tilesNeeded = tottalBathroomArea / tileArea;
+
+Console.WriteLine(Math.Round(tilesNeeded));
