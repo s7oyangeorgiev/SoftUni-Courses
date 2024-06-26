ea3f90e45cea37207c1824cde4d9f6f241d2268a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:25:38 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/02. CondenseArrayToNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+while (numbers.Count > 1)
+{
+    List<int> condensed = new List<int>();
+
+    for (int i = 0; i < numbers.Count - 1; i++)
+    {
+        condensed.Add(numbers[i] + numbers[i + 1]);
+    }
+
+    numbers = condensed;
+}
+Console.WriteLine(numbers[0]);
