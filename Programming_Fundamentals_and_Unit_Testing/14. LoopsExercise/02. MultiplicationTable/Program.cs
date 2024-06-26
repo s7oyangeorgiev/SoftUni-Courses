e410efcdfc8a01c2e53105e9f977d2a78b9ae69a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:20:49 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/02. MultiplicationTable/Program.cs
@@ -0,0 +1,11 @@
+﻿int numN = int.Parse(Console.ReadLine());
+
+int multiplication = 1;
+int result = numN * multiplication;
+
+while (multiplication <= 10)
+{
+    Console.WriteLine($"{numN} x {multiplication} = {result}");
+    multiplication++;
+    result = numN * multiplication;
+}
