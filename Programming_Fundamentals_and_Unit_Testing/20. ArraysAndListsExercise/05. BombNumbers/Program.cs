65b0d76106badeeb078ea091411cdf37a06c9fd2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:27:12 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/05. BombNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+int[] bombInfo = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int bombNumber = bombInfo[0];
+int bombPower = bombInfo[1];
+
+while (numbers.Contains(bombNumber))
+{
+    int bombIndex = numbers.IndexOf(bombNumber);
+
+    int startIndex = Math.Max(0, bombIndex - bombPower);
+    int endIndex = Math.Min(numbers.Count - 1, bombIndex + bombPower);
+
+    numbers.RemoveRange(startIndex, endIndex - startIndex + 1);
+}
+int sum = numbers.Sum();
+
+Console.WriteLine(sum);
