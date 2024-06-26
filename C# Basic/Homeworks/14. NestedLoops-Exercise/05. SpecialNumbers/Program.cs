69b3595fa74ccbce84ea4694f2a704c61d95faf0
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:53:22 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/14. NestedLoops-Exercise/05. SpecialNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int num = int.Parse(Console.ReadLine());
+
+
+
+for (int i = 1111; i <= 9999; i++)
+{
+    string divisor = i.ToString();
+
+    bool isSpecial = true;
+
+    for (int j = 0; j < divisor.Length; j++)
+    {
+        int currentDivisor = int.Parse(divisor[j].ToString());
+
+        if (currentDivisor == 0)
+        {
+            isSpecial = false;
+            break;
+        }
+
+        if (num % currentDivisor == 0)
+        {
+            continue;
+        }
+        else
+        {
+            isSpecial = false;
+            break;
+        }
+
+
+    }
+    if (isSpecial)
+    {
+        Console.Write(i + " ");
+    }
+}
