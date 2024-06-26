c9b38dfaffbe3e8757994e71df484a3edcf27c56
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:17:38 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/13. WhileLoopLab/04. Sequence2k+1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+int maxNum = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= maxNum; i++)
+{
+    while (i <= maxNum)
+    {
+        Console.WriteLine(i);
+        i = i + i + 1;
+    }
+}
