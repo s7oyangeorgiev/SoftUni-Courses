f8f032c75c82baa9d250828c0ad9e3e25385dfc1
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:40:54 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/11. WhileLoop-Lab/05. AccountBalance/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string money = Console.ReadLine();
+
+double sum = 0;
+
+while (money != "NoMoreMoney")
+{
+    double currentMoney = double.Parse(money);
+
+    if (currentMoney < 0)
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+    Console.WriteLine($"Increase: {currentMoney:f2}");
+    sum += currentMoney;
+    money = Console.ReadLine();
+}
+Console.WriteLine($"Total: {sum:f2}");
