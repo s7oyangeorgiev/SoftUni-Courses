e31d1cce1b76020b97693be700c344a92c3a5239
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:20:14 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/18. ArraysLab/04. EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int oddSum = 0;
+int evenSum = 0;
+
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        evenSum += number;
+    }
+    else
+    {
+        oddSum += number;
+    }
+}
+
+int sumDifference = (evenSum - oddSum);
+Console.WriteLine(sumDifference);
+
+
+
