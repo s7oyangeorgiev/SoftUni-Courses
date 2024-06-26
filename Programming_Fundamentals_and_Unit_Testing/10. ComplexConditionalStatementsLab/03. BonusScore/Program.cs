50c3a048917845edf6b6cd2110fdaf9af3906d7b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:07:59 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/10. ComplexConditionalStatementsLab/03. BonusScore/Program.cs
@@ -0,0 +1,18 @@
+﻿int points = int.Parse(Console.ReadLine());
+
+int totalPoints = points;
+
+if ( points >= 0 && points <= 3)
+{
+    totalPoints += 5;
+}
+else if ( points >= 4 && points <= 6)
+{
+    totalPoints += 15;
+}
+else if (points >= 7 && points <= 9)
+{
+    totalPoints += 20;
+}
+
+Console.WriteLine($"{totalPoints}");
