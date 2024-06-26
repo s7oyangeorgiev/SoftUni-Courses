140bde1a4f41d4421afed5565ba84bee840ab433
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:22:41 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/05. RepeatString/Program.cs
@@ -0,0 +1,14 @@
+﻿string text = Console.ReadLine();
+int repeatCount = int.Parse(Console.ReadLine());
+string result = RepeatText(text, repeatCount);
+Console.WriteLine(result);
+
+static string RepeatText(string text, int count)
+{
+    string result = "";
+    for (int i = 1; i <= count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
