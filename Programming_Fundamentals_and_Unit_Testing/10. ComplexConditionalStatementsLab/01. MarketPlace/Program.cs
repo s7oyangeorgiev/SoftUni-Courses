0a5adc7fc35f5ca064126306f71c177d10da3d31
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:06:56 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/10. ComplexConditionalStatementsLab/01. MarketPlace/Program.cs
@@ -0,0 +1,40 @@
+﻿string product = Console.ReadLine();
+string day = Console.ReadLine();
+
+double price = 0;
+
+if (product == "Banana")
+{
+    if (day == "Weekday")
+    {
+        price += 2.50;
+    }
+    else
+    {
+        price += 2.70;
+    }
+}
+else if (product == "Apple")
+{
+    if (day == "Weekday")
+    {
+        price += 1.30;
+    }
+    else
+    {
+        price += 1.60;
+    }
+}
+else if (product == "Kiwi")
+{
+    if (day == "Weekday")
+    {
+        price += 2.20;
+    }
+    else
+    {
+        price += 3.00;
+    }
+}
+
+Console.WriteLine($"{price:f2}");
