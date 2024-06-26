7044940686f89e8a0258e399306e3046fae5c969
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:21:47 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/04. VowelSum/Program.cs
@@ -0,0 +1,20 @@
+﻿int numCount = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i <= numCount; i++)
+{
+    char n = char.Parse(Console.ReadLine());
+
+    switch (n)
+    {
+        case 'a': sum += 1; break;
+        case 'e': sum += 2; break;
+        case 'i': sum += 3; break;
+        case 'o': sum += 4; break;
+        case 'u': sum += 5; break;
+        default: sum += 0; break;
+    }
+}
+
+Console.WriteLine(sum);
