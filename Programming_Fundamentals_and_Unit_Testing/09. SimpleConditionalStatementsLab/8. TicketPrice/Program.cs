93b889753e0e627f0c155e16f6ae6bccabe4d6d9
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:04:13 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/09. SimpleConditionalStatementsLab/8. TicketPrice/Program.cs
@@ -0,0 +1,17 @@
+﻿string ticketType = Console.ReadLine();
+
+double studentPrice = 1.00;
+double regularPrice = 1.60;
+
+if (ticketType == "student")
+{
+    Console.WriteLine($"${studentPrice:f2}");
+}
+else if (ticketType == "regular")
+{
+    Console.WriteLine($"${regularPrice:f2}");
+}
+else
+{
+    Console.WriteLine("Invalid ticket type!");
+}
