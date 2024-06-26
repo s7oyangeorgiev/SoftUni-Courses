d932a373e8a7036a2ce4e119d6d5fd7798028b5e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:04:44 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/06. ConditionalStatementsExercise/06. WorldSwimmingRecord/Program.cs
@@ -0,0 +1,17 @@
+﻿double recordInSeconds = double.Parse(Console.ReadLine());
+double distanceInMeters = double.Parse(Console.ReadLine());
+double timeInSecondsForOneMeterSwimming = double.Parse(Console.ReadLine());
+
+double timeForAllDistance = distanceInMeters * timeInSecondsForOneMeterSwimming;
+double delayTime = Math.Floor(distanceInMeters / 15) * 12.5;
+double totalTime = timeForAllDistance + delayTime;
+
+if (recordInSeconds > totalTime)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+}
+else
+{
+    double timeNeededForNewRecord = totalTime - recordInSeconds;
+    Console.WriteLine($"No, he failed! He was {timeNeededForNewRecord:f2} seconds slower.");
+}
