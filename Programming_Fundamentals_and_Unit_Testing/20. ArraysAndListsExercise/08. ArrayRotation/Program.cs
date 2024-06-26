7241ecc230ffc9f628028fae23d1043fa3ebadc3
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:28:36 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/08. ArrayRotation/Program.cs
@@ -0,0 +1,14 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int firstElement = numbers[0];
+    numbers.RemoveAt(0);
+    numbers.Add(firstElement);
+}
+Console.WriteLine(string.Join(" ", numbers));
