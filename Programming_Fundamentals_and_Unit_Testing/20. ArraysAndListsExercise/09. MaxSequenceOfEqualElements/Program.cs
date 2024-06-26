62c191bded98534891f1795641227779ca8fba68
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:29:05 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/09. MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,40 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+int longestSequenceStart = 0;
+int longestSequenceLength = 1;
+
+int currentSequenceStart = 0;
+int currentSequenceLength = 1;
+
+for (int i = 1; i < numbers.Count; i++)
+{
+    if (numbers[i] == numbers[i - 1])
+    {
+        currentSequenceLength++;
+    }
+    else
+    {
+        if (currentSequenceLength > longestSequenceLength)
+        {
+            longestSequenceLength = currentSequenceLength;
+            longestSequenceStart = currentSequenceStart;
+        }
+
+        currentSequenceStart = i;
+        currentSequenceLength = 1;
+    }
+}
+
+if (currentSequenceLength > longestSequenceLength)
+{
+    longestSequenceLength = currentSequenceLength;
+    longestSequenceStart = currentSequenceStart;
+}
+
+for (int i = 0; i < longestSequenceLength; i++)
+{
+    Console.Write(numbers[longestSequenceStart] + " ");
+}
