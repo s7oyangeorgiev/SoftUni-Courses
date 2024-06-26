5d7ce3ee5c9fabc7aae31d0e67b37c4211ecb4e5
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:17:11 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/13. WhileLoopLab/03. OddNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿
+while (true)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num % 2 == 0)
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine(num);
+        break;
+    }
+}
