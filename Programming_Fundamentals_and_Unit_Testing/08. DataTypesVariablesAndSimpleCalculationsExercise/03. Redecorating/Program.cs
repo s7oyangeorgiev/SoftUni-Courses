e82ea4c65cf8d88bdfe25e36b826354cb3d986e2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:57:54 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/08. DataTypesVariablesAndSimpleCalculationsExercise/03. Redecorating/Program.cs
@@ -0,0 +1,14 @@
+﻿int nylonNeeded = int.Parse(Console.ReadLine());
+int paintNeeded = int.Parse(Console.ReadLine());
+int thinnerNeeded = int.Parse(Console.ReadLine());
+int hoursNeeded = int.Parse(Console.ReadLine());
+
+double nylonCosts = (nylonNeeded + 2) * 1.50;
+double paintCosts = (paintNeeded + (paintNeeded * 0.10)) * 14.50;
+double thinnerCosts = thinnerNeeded * 5.00;
+double bagCosts = 0.40;
+double totalMaterialCosts = nylonCosts + paintCosts + thinnerCosts + bagCosts;
+double craftsmen = (totalMaterialCosts * 0.30) * hoursNeeded;
+double totalCosts = totalMaterialCosts + craftsmen;
+
+Console.WriteLine($"{totalCosts}");
