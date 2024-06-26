0a3fab6b8ce0e4d14da86469cc57263f8bdd165f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:54:56 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/07. SimpleCalculationsLab/05. DepositCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿double depositedAmount = double.Parse(Console.ReadLine());
+int depositInMonths = int.Parse(Console.ReadLine());
+double interestRate =  double.Parse(Console.ReadLine());
+
+double accumulatedInterest = depositedAmount * interestRate / 100;
+double interestForOneMonth = accumulatedInterest / 12;
+double amount = depositedAmount + depositInMonths * interestForOneMonth;
+
+Console.WriteLine($"{amount:f2}");
