8a2539803bf94aad5edf936db395f191afa3ecb2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:47:10 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/05. SimpleCommandsAndVisualStudioExercise/04. TrapezoidArea/Program.cs
@@ -0,0 +1,5 @@
+﻿int firstBase = int.Parse(Console.ReadLine());
+int secondBase = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{(firstBase + secondBase) / 2 * height}");
