e48f3a381a6575cffeaeea06cc983f388521a9cf
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:51:35 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/06. DataTypesAndVariablesLab/06. PetsFood/Program.cs
@@ -0,0 +1,8 @@
+﻿int dogFoodCount = int.Parse(Console.ReadLine());
+int catFoodCount = int.Parse(Console.ReadLine());
+
+double dogFoodCosts = dogFoodCount * 2.50;
+double catFoodCosts = catFoodCount * 4.00;
+double tottalCosts = dogFoodCosts + catFoodCosts;
+
+Console.WriteLine($"{tottalCosts:f2} lv.");
