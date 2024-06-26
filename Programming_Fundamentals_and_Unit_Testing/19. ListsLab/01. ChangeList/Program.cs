614cab11e0f2073b96ea7e1e5135a9e372cd9c85
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:22:13 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/19. ListsLab/01. ChangeList/Program.cs
@@ -0,0 +1,32 @@
+﻿List<int> listOfNumbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "end")
+    {
+        break;
+    }
+
+    string[] partsOfCommand = command.Split(" ");
+
+    if (partsOfCommand[0] == "Delete")
+    {
+        int element = int.Parse(partsOfCommand[1]);
+        listOfNumbers.RemoveAll(e => e == element);
+    }
+    else if (partsOfCommand[0] == "Insert")
+    {
+        int element = int.Parse(partsOfCommand[1]);
+        int position = int.Parse(partsOfCommand[2]);
+        if (position >= 0 && position <= listOfNumbers.Count)
+        {
+            listOfNumbers.Insert(position, element);
+        }
+    }
+}
+Console.WriteLine(string.Join(" ", listOfNumbers));
