99cd847bf7a8fe62fcd962351e1bfd124735fc9d
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:56:17 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/04.FirstStepsInCodingExercise/09.FishTank/Program.cs
@@ -0,0 +1,13 @@
+﻿int lengthInSantimeters = int.Parse(Console.ReadLine());
+int widthInSantimeters = int.Parse(Console.ReadLine());
+int heightInSantimeters = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+double aquariumVolume = lengthInSantimeters * widthInSantimeters * heightInSantimeters;
+double volumeInLitters = aquariumVolume * 0.001;
+double percentOccupied = percent / 100;
+double litersNeeded = volumeInLitters * (1 - percentOccupied);
+
+Console.WriteLine(litersNeeded);
+
+
