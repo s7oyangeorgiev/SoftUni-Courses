b52fd82935435b6ae404f569772cd6d5451c8f78
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:22:32 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/19. ListsLab/02. ListOfProducts/Program.cs
@@ -0,0 +1,20 @@
+﻿// Read the number of products
+int n = int.Parse(Console.ReadLine());
+
+// Create a list to store the products
+List<string> products = new List<string>();
+
+// Read n lines of products
+for (int i = 0; i < n; i++)
+{
+    products.Add(Console.ReadLine());
+}
+
+// Sort the products by name
+products.Sort();
+
+// Print the numbered list of products
+for (int i = 0; i < products.Count; i++)
+{
+    Console.WriteLine($"{i + 1}.{products[i]}");
+}
