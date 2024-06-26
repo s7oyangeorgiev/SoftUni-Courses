ec8d443b1a9be2fd75c5842d3f6a172f43787cee
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:41:22 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/11. WhileLoop-Lab/06. MaxNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string number = Console.ReadLine();
+
+int maxNumber = int.MinValue;
+
+while (number != "Stop")
+{
+    int currentNumber = int.Parse(number);
+
+    if (currentNumber > maxNumber)
+    {
+        maxNumber = currentNumber;
+    }
+    number = Console.ReadLine();
+}
+Console.WriteLine(maxNumber);
