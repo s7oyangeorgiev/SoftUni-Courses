9762d6c6653a540d57241374bc000c391f41c426
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:23:35 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/07. GreaterOfTwoValues/Program.cs
@@ -0,0 +1,67 @@
+﻿string type = Console.ReadLine();
+
+if (type == "int")
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine(GetIntMax(a, b));
+
+}
+else if (type == "char")
+{
+    char a = char.Parse(Console.ReadLine());
+    char b = char.Parse(Console.ReadLine());
+    Console.WriteLine(GetCharMax(a, b));
+}
+else
+{
+    string a = Console.ReadLine();
+    string b = Console.ReadLine();
+    Console.WriteLine(GetStringMax(a, b));
+
+
+}
+static int GetIntMax(int a, int b)
+{
+    int result = 0;
+    if (a > b)
+    {
+        result = a;
+    }
+    else
+    {
+        result = b;
+    }
+
+    return result;
+}
+
+static char GetCharMax(char a, char b)
+{
+    char result = ' ';
+    if (a > b)
+    {
+        result = a;
+    }
+    else
+    {
+        result = b;
+    }
+
+    return result;
+}
+
+static string GetStringMax(string a, string b)
+{
+    int result = a.CompareTo(b);
+
+    if (result > 0)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+   
+}
