d36ff28492627ae79afb42c6eccaf148e4fb20ef
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:51:28 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/11. SimpleAndComplexConditionalStatementsExercise/04. NumberOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿double numOne = double.Parse(Console.ReadLine());
+double numTwo = double.Parse(Console.ReadLine());
+string operation =  Console.ReadLine();
+
+double result = 0;
+switch (operation)
+{
+    case "+": result += numOne + numTwo; break;
+    case "-": result += numOne - numTwo; break;
+    case "*": result += numOne * numTwo; break;
+    case "/": result += numOne / numTwo; break;
+}
+
+Console.WriteLine($"{numOne} {operation} {numTwo} = {result:f2}");
