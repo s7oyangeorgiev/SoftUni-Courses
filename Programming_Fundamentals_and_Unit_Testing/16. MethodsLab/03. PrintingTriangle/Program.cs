4da2026838d4c2ba436b13a6377cbdcc50df0368
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:21:43 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/03. PrintingTriangle/Program.cs
@@ -0,0 +1,23 @@
+﻿int num = int.Parse(Console.ReadLine());
+PrintingTriangle(num);
+
+static void PrintingTriangle(int num)
+{
+    for (int row = 1; row <= num; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            Console.Write(col + " ");
+        }
+        Console.WriteLine();
+    }
+
+    for (int row = num - 1;row >= 1; row--)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            Console.Write(col + " ");
+        }
+        Console.WriteLine();
+    }
+}
