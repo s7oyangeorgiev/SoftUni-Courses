3537899fab002ebabe1cac421c4d73f9bc58f738
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:44:07 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/04. VisualStudioLab/03. CustomerInfo/Program.cs
@@ -0,0 +1,5 @@
+﻿string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string email = Console.ReadLine();
+
+Console.WriteLine($"Customer: {firstName} {lastName} ({email})");
