cf61ad22ac28b6bd9172d8ae6f1e9492af890975
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:55:27 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/07. SimpleCalculationsLab/06. MandatoryLiterature/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberOfPages = int.Parse(Console.ReadLine());
+int pagesReadForOneHour =  int.Parse(Console.ReadLine());
+int daysCount =  int.Parse(Console.ReadLine());
+
+int totalReadingTime = numberOfPages / pagesReadForOneHour;
+int requiredHoursPerDay = totalReadingTime / daysCount;
+
+Console.WriteLine(requiredHoursPerDay);
