bfeb461a4f451a348a8e05f5ec8e5e28e7d30c55
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:18:07 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/13. WhileLoopLab/05. SumDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿//int num = int.Parse(Console.ReadLine());
+
+//int sum = num.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum();
+
+//Console.WriteLine(sum);
+
+int num = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (num > 0)
+{
+    int lastDigit = num % 10;
+    sum += lastDigit;
+    num /= 10;
+}
+
+Console.WriteLine(sum);
