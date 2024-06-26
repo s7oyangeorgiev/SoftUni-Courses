75351b2b934c82a18bb37687d651d0e98e7b75c9
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:25:02 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/01. GaussTrick/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToList();
+
+List<int> sums = new List<int>();
+
+for (int i = 0; i < numbers.Count / 2; i++)
+{
+    int sum = numbers[i] + numbers[numbers.Count - 1 - i];
+    sums.Add(sum);
+}
+
+if (numbers.Count % 2 != 0)
+{
+    sums.Add(numbers[numbers.Count / 2]);
+}
+Console.WriteLine(string.Join(" ", sums));
