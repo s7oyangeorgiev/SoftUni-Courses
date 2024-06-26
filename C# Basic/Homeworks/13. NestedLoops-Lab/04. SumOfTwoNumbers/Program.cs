23e625eef34f9eadc3a8489cae635d5ae1c5e08d
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:49:13 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/13. NestedLoops-Lab/04. SumOfTwoNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+int magicNumber = int.Parse(Console.ReadLine());
+
+int combination = 0;
+bool isMagic = false;
+
+for (int i = start; i <= end; i++)
+{
+    for (int j = start; j <= end; j++)
+    {
+        combination++;
+
+        if (i + j == magicNumber)
+        {
+            isMagic = true;
+            Console.WriteLine($"Combination N:{combination} ({i} + {j} = {magicNumber})");
+            break;
+        }
+    }
+
+    if (isMagic)
+    {
+        break;
+    }
+}
+if (!isMagic)
+{
+    Console.WriteLine($"{combination} combinations - neither equals {magicNumber}");
+}
