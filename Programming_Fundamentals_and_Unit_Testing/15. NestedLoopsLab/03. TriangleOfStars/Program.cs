0e178cfdc92cfcbde9df019adf27cca4ddd879b1
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:28:53 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/15. NestedLoopsLab/03. TriangleOfStars/Program.cs
@@ -0,0 +1,11 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+for (int rows = 1; rows <= size; rows++)
+{
+    for (int stars = 1; stars <= rows; stars++)
+    {
+        string star = "*";
+        Console.Write(star);
+    }
+    Console.WriteLine();
+}
