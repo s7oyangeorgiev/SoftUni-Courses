dba0586f71e8ffed21cf5c9f56602d32f5a3d043
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:23:37 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/08. AccountBalance/Program.cs
@@ -0,0 +1,24 @@
+﻿string incomesOrExpenses = Console.ReadLine();
+
+double balance = 0;
+
+while (incomesOrExpenses != "End")
+{
+    double num = double.Parse(incomesOrExpenses);
+
+    if (num >= 0)
+    {
+        Console.WriteLine($"Increase: {num:f2}");
+        balance += num;
+    }
+    else
+    {
+        Console.WriteLine($"Decrease: {num * -1:f2}");
+        balance += num;
+    }
+
+    incomesOrExpenses = Console.ReadLine();
+
+}
+
+Console.WriteLine($"Balance: {balance:f2}");
