353f5ea33da7d568598ad3d25e511bc32fc4022f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:29:30 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/09. ForLoop-Lab/09. LeftAndRightSum/Program.cs
@@ -0,0 +1,28 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sumUp = 0;
+int sumDown = 0;
+
+for (int i = 0; i < number; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    sumUp += currentNumber;
+}
+
+for (int i = 0; i < number; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    sumDown += currentNumber;
+}
+
+if (sumUp == sumDown)
+{
+    Console.WriteLine($"Yes, sum = {sumDown}");
+}
+else
+{
+    Console.WriteLine($"No, diff = {Math.Abs(sumUp - sumDown)}");
+}
+
+
+
