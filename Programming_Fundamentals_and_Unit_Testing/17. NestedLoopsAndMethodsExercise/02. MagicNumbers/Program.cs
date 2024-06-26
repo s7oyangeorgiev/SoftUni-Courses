dbb0b589d644691d53b309d1712db2843c64430f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:14:14 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/02. MagicNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+for  (int i = 1; i <= num; i++)
+{
+    for (int j = 1; j <= num; j++)
+    {
+        for (int k = 1; k <= num; k++)
+        {
+            if ( i >= 10 || j >= 10 || k >= 10)
+            {
+                continue;
+            }
+            if (i * j * k == num)
+            {
+                Console.Write($"{i}{j}{k} ");
+            }
+        }
+    }
+}
+Console.WriteLine();
