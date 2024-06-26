e3ec465c55fc02a9a44098f0ddd77e21d8b3b428
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:36:41 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/10. ForLoop-Exercise/06. Oscars/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string actorName = Console.ReadLine();
+double pointsFromAcademy = double.Parse(Console.ReadLine());
+int evaluative = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < evaluative; i++)
+{
+    string evaluativeName = Console.ReadLine();
+    double pointsFromEvaluative = double.Parse(Console.ReadLine());
+
+    pointsFromAcademy += evaluativeName.Length * pointsFromEvaluative / 2;
+
+    if (pointsFromAcademy > 1250.5)
+    {
+        break;
+    }
+}
+if (pointsFromAcademy > 1250.5)
+{
+    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {pointsFromAcademy:f1}!");
+}
+else
+{
+    Console.WriteLine($"Sorry, {actorName} you need {1250.5 - pointsFromAcademy:f1} more!");
+}
