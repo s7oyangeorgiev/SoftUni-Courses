234099c7feafff98362731a519c64685e600318a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:53:01 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/14. NestedLoops-Exercise/04. TrainTheTrainers/Program.cs
@@ -0,0 +1,34 @@
+﻿int peopleInJury = int.Parse(Console.ReadLine());
+
+double allPresentationTottalGrade = 0;
+int presentationCount = 0;
+
+while (true)
+{
+    string nameOfPresentation = Console.ReadLine();
+
+    if (nameOfPresentation == "Finish")
+    {
+        break;
+    }
+    presentationCount++;
+
+    double tottalGrade = 0;
+
+    for (int i = 0; i < peopleInJury; i++)
+    {
+        double grade = double.Parse(Console.ReadLine());
+        tottalGrade += grade;
+
+    }
+
+    double avaragePresentationGrade = tottalGrade / peopleInJury;
+    allPresentationTottalGrade += avaragePresentationGrade;
+    Console.WriteLine($"{nameOfPresentation} - {avaragePresentationGrade:f2}.");
+}
+Console.WriteLine($"Student's final assessment is {allPresentationTottalGrade / presentationCount:f2}.");
+
+
+
+
+
