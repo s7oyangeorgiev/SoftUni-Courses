9c01a0585e7b3c066f9f159f44c3ad5ff06a91d2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:50:13 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/13. NestedLoops-Lab/06. Building/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int countOfFloors = int.Parse(Console.ReadLine());
+int countOfRooms = int.Parse(Console.ReadLine());
+
+for (int floors = countOfFloors; floors >= 1; floors--)
+{
+    for (int room = 0; room < countOfRooms; room++)
+    {
+        if (floors == countOfFloors)
+        {
+            Console.Write($"L{floors}{room} ");
+        }
+        else if (floors % 2 == 0) 
+        {
+            Console.Write($"O{floors}{room} ");
+        }
+        else if (floors % 2 == 1)
+        {
+            Console.Write($"A{floors}{room} ");
+        }
+    }
+
+    Console.WriteLine();
+
+}
