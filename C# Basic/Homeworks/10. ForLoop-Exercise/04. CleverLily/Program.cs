96b9946a8a3b95dfa5e9e2189a65e1aefd756bc2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:32:50 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/10. ForLoop-Exercise/04. CleverLily/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int age = int.Parse(Console.ReadLine());
+double washingMashinePrice = double.Parse(Console.ReadLine());
+int priceForOneToy = int.Parse(Console.ReadLine());
+
+double sum = 0;
+int moneyToBeRecieved = 10;
+
+for (int currAge = 1; currAge <= age; currAge++)
+{
+    if (currAge % 2 == 0)
+    {
+        sum += moneyToBeRecieved - 1;
+        moneyToBeRecieved += 10;
+    }
+    else
+    {
+        sum += priceForOneToy;
+    }
+}
+if (sum >= washingMashinePrice)
+{
+    Console.WriteLine($"Yes! {sum - washingMashinePrice:f2}");
+}
+else
+{
+    Console.WriteLine($"No! {washingMashinePrice - sum:f2}");
+}
