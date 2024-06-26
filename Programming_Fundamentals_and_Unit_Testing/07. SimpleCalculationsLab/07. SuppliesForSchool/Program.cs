a699c783e1de4a5993dce95d1c36a193fc065916
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:55:54 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/07. SimpleCalculationsLab/07. SuppliesForSchool/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberOfPensPackages = int.Parse(Console.ReadLine());
+int numberOfMarkersPackets =  int.Parse(Console.ReadLine());
+int litersOfBoarCleaner = int.Parse(Console.ReadLine());
+int discountPercentage =  int.Parse(Console.ReadLine());
+
+double pensPrice = numberOfPensPackages * 5.80;
+double markersPrice = numberOfMarkersPackets * 7.20;
+double boarCleanerPrice = litersOfBoarCleaner * 1.20;
+
+double totalPrice = pensPrice + markersPrice + boarCleanerPrice;
+double priceAfterDiscount = totalPrice - (totalPrice * (discountPercentage / 100.00));
+
+Console.WriteLine(priceAfterDiscount);
