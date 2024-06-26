d3e2da2b9e4c9bd4184af9b99325cdccb157d194
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:56:43 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/12. ForLoopLab/03. SumNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+int countOfNumbers = int.Parse(Console.ReadLine());
+
+double result = 0;
+
+for (int i = 0; i < countOfNumbers; i++)
+{
+    double num = double.Parse(Console.ReadLine());
+    result += num;
+}
+
+Console.WriteLine(result);
