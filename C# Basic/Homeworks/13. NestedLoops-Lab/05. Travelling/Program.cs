3fab3af507b9d67a4d68ac9dab14ff422623facb
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:49:45 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/13. NestedLoops-Lab/05. Travelling/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string destination = Console.ReadLine();
+
+while (destination != "End")
+{
+    double budget = double.Parse(Console.ReadLine());
+    double savingMoney = 0;
+    
+    while (savingMoney < budget)
+    {
+        double currentMoney = double.Parse(Console.ReadLine());
+        savingMoney += currentMoney;
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+
+
+    destination = Console.ReadLine();
+}
+    
+
