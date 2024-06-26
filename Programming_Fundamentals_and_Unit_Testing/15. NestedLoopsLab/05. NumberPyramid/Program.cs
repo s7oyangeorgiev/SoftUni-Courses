2376f6d23e1e02d09a451b6520b8be4f303ab69a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:29:50 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/15. NestedLoopsLab/05. NumberPyramid/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int currentNum = 1;
+
+for (int rows = 1; currentNum <= num; rows++)
+{
+    for (int cols = 1; cols <= rows && currentNum <= num; cols++)
+    {
+        Console.Write($"{currentNum} ");
+        currentNum++;
+    }
+    Console.WriteLine();
+}
