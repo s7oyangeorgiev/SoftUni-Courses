29483c07b8d566d73c44380d78c3bb6a059806b7
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:53:26 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/04.FirstStepsInCodingExercise/03.DepositCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿double depositAmount = double.Parse(Console.ReadLine());   
+int termDepositInMounth = int.Parse(Console.ReadLine());
+double annualInterestRate = double.Parse(Console.ReadLine());
+
+double accruedInterest = depositAmount * (annualInterestRate / 100);
+double interestForMount = accruedInterest / 12;
+
+double tottalSum = depositAmount + termDepositInMounth * interestForMount;
+
+Console.WriteLine(tottalSum);
+
