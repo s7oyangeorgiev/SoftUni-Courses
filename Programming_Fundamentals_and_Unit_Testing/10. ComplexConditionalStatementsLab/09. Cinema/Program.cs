b832072a2cfc6a4011b86708ef60d2132960cadd
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:10:48 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/10. ComplexConditionalStatementsLab/09. Cinema/Program.cs
@@ -0,0 +1,23 @@
+﻿string typeOfMovie = Console.ReadLine();
+int countOfRows = int.Parse(Console.ReadLine());
+int countOfSeatsPerRow =  int.Parse(Console.ReadLine());
+
+double pricePerSeat = 0;
+
+if (typeOfMovie == "Premiere")
+{
+    pricePerSeat += 12.00;
+}
+else if (typeOfMovie == "Normal")
+{
+    pricePerSeat += 7.50;
+}
+else if (typeOfMovie == "Discount")
+{
+    pricePerSeat += 5.00;
+}
+
+double totalSeats = countOfRows * countOfSeatsPerRow;
+double totalPriceForAllSeats = totalSeats * pricePerSeat;
+
+Console.WriteLine($"{totalPriceForAllSeats:f2}");
