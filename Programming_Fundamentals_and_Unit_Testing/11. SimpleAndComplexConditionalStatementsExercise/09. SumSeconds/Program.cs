162c0e86bc82c64c32f13c20e2899527e19cb113
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:54:05 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/11. SimpleAndComplexConditionalStatementsExercise/09. SumSeconds/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstAthleteTimes = int.Parse(Console.ReadLine());
+int secondAthleteTime =  int.Parse(Console.ReadLine());
+int thirdAthleteTime = int.Parse(Console.ReadLine());
+
+int totalAthletesTime = firstAthleteTimes + secondAthleteTime + thirdAthleteTime;
+
+int totalTimeInMinutes = totalAthletesTime / 60;
+double totalTimeInSeconds = totalAthletesTime % 60.00;
+
+if ( totalTimeInSeconds < 10)
+{
+    Console.WriteLine($"{totalTimeInMinutes}:0{totalTimeInSeconds}");
+}
+else
+{
+    Console.WriteLine($"{totalTimeInMinutes}:{totalTimeInSeconds}");
+}
