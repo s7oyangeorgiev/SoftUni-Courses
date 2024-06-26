f0688a480e36d7dbae8b2b9fa822aa616ec4764c
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:27:38 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/06. CardsGame/Program.cs
@@ -0,0 +1,45 @@
+﻿Queue<int> player1 = new Queue<int>(Array.ConvertAll(Console.ReadLine()
+    .Split(" "), int.Parse));
+Queue<int> player2 = new Queue<int>(Array.ConvertAll(Console.ReadLine()
+    .Split(" "), int.Parse));
+
+while (player1.Count > 0 && player2.Count > 0)
+{
+    int card1 = player1.Dequeue();
+    int card2 = player2.Dequeue();
+
+    if (card1 > card2)
+    {
+        player1.Enqueue(card1);
+        player1.Enqueue(card2);
+    }
+    else if (card2 > card1)
+    {
+        player2.Enqueue(card2);
+        player2.Enqueue(card1);
+    }
+}
+
+if (player1.Count > 0)
+{
+    int sum = CalculateSum(player1);
+    Console.WriteLine($"First player wins! Sum: {sum}");
+}
+else if (player2.Count > 0)
+{
+    int sum = CalculateSum(player2);
+    Console.WriteLine($"Second player wins! Sum: {sum}");
+}
+else
+{
+    Console.WriteLine("No winner");
+}
+    static int CalculateSum(Queue<int> queue)
+{
+    int sum = 0;
+    foreach (int card in queue)
+    {
+        sum += card;
+    }
+    return sum;
+}
