8fd5ac017a41e7a0615e0e58511c2c18cfc80179
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:58:07 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/05.ConditionalStatementsLab/02.GreaterNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberOne = int.Parse(Console.ReadLine());
+int numberTwo = int.Parse(Console.ReadLine());
+
+if (numberOne > numberTwo)
+{
+    Console.WriteLine(numberOne);
+}
+else
+{
+    Console.WriteLine(numberTwo);
+}
