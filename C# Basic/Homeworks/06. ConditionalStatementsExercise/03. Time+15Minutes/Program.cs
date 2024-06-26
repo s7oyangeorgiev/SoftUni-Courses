ef903758d357ba4188a07177746d1b758c4a6900
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:03:21 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/06. ConditionalStatementsExercise/03. Time+15Minutes/Program.cs
@@ -0,0 +1,16 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+int tottalMinutes = hours * 60 + minutes + 15;
+int newHours = (tottalMinutes / 60) % 24;
+int newMinutes = tottalMinutes % 60;
+
+if (newMinutes < 10)
+{
+    Console.WriteLine($"{newHours}:0{newMinutes}");
+}
+else
+{
+    Console.WriteLine($"{newHours}:{newMinutes}");
+}
+
