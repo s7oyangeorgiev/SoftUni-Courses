26372a8fde22bbc4d7ca23f94203b7b5aea3acff
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:30:47 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/15. NestedLoopsLab/07. SumOfDigitsCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿string num = "";
+
+while ( num != "End")
+{
+    string currentNum = Console.ReadLine();
+    
+    if (currentNum == "End")
+    {
+        Console.WriteLine("Goodbye");
+        break;
+    }
+    int digitsNum = int.Parse(currentNum);
+    
+    int sum = currentNum.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum();
+    Console.WriteLine($"Sum of digits = {sum}");
+}
+
+
+
+//while (true)
+//{
+//    string currentNum = Console.ReadLine();
+
+//    if (currentNum == "End")
+//    {
+//        Console.WriteLine("Goodbye");
+//        break;
+//    }
+//    int digitsNum = int.Parse(currentNum);
+
+//    int sum = currentNum.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum();
+//    Console.WriteLine($"Sum of digits = {sum}");
+//}
