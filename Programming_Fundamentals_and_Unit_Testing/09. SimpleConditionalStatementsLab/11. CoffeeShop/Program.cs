5a1a28dc65c28e608f1f5483c35d708ba3f1132b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 10:05:47 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/09. SimpleConditionalStatementsLab/11. CoffeeShop/Program.cs
@@ -0,0 +1,20 @@
+﻿string drinkName = Console.ReadLine();
+string extra = Console.ReadLine();
+
+double price = 0;
+
+if ( drinkName == "coffee")
+{
+    price += 1.00;
+}
+else if ( drinkName == "tea")
+{
+    price += 0.60;
+}
+
+if ( extra == "sugar")
+{
+    price += 0.40;
+}
+
+Console.WriteLine($"Final price: ${price:f2}");
