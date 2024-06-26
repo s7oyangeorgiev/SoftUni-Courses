a6a18e649c5729e35f7503673895e0f2c4a9488f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:45:31 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/12. WhileLoop-Exercise/05. Coins/Program.cs
@@ -0,0 +1,49 @@
+﻿
+decimal sum = decimal.Parse(Console.ReadLine());
+
+int coinCounter = 0;
+
+while (sum > 0)
+{
+    if (sum >= 2m)
+    {
+        sum -= 2m;
+        coinCounter++;
+    }
+    else if (sum >= 1m)
+    {
+        sum -= 1m;
+        coinCounter++;
+    }
+    else if (sum >= 0.50m)
+    {
+        sum -= 0.50m;
+        coinCounter++;
+    }
+    else if (sum >= 0.20m)
+    {
+        sum -= 0.20m;
+        coinCounter++;
+    }
+    else if (sum >= 0.10m)
+    {
+        sum -= 0.10m;
+        coinCounter++;
+    }
+    else if (sum >= 0.05m)
+    {
+        sum -= 0.05m;
+        coinCounter++;
+    }
+    else if (sum >= 0.02m)
+    {
+        sum -= 0.02m;
+        coinCounter++;
+    }
+    else
+    {
+        sum -= 0.01m;
+        coinCounter++;
+    }
+}
+Console.WriteLine(coinCounter);
