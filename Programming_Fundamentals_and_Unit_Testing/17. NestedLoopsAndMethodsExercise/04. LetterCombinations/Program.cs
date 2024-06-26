8e26807c46904e97884689671b7ba030bde4ac9c
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:15:04 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/04. LetterCombinations/Program.cs
@@ -0,0 +1,26 @@
+﻿char startLetterS = char.Parse(Console.ReadLine());
+char endLetterE = char.Parse(Console.ReadLine());
+char excludedLetterX =  char.Parse(Console.ReadLine());
+
+int combinationCount = 0;
+
+for (char i = startLetterS; i <= endLetterE; i++)
+{
+	for (char j = startLetterS; j <= endLetterE; j++)
+	{
+		for (char k = startLetterS; k <= endLetterE; k++)
+		{
+			if ( i == excludedLetterX || j == excludedLetterX || k == excludedLetterX )
+			{
+				continue;
+			}
+			else
+			{
+                Console.Write($"{i}{j}{k} ");
+				combinationCount++;
+            }
+		}
+	}
+}
+Console.WriteLine();
+Console.WriteLine(combinationCount);
