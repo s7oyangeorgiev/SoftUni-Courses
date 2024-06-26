85365c716c3185dfe9cd39196a717fe40e375f8a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:51:59 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/11. SimpleAndComplexConditionalStatementsExercise/05. VacationExpenses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+string season = Console.ReadLine();
+string accommodationType =  Console.ReadLine();
+int countOfTheDays = int.Parse(Console.ReadLine());
+
+double price = 0;
+
+if ( accommodationType == "Hotel")
+{
+    switch (season)
+    {
+        case "Spring": price += (countOfTheDays * 30) * 0.80; break;
+        case "Summer": price += (countOfTheDays * 50); break;
+        case "Autumn": price += (countOfTheDays * 20) * 0.70; break;
+        case "Winter": price += (countOfTheDays * 40) * 0.90; break;
+    }
+}
+else if ( accommodationType == "Camping")
+{
+    switch (season)
+    {
+        case "Spring": price += (countOfTheDays * 10) * 0.80; break;
+        case "Summer": price += (countOfTheDays * 30); break;
+        case "Autumn": price += (countOfTheDays * 15) * 0.70; break;
+        case "Winter": price += (countOfTheDays * 10) * 0.90; break;
+    }
+}
+
+Console.WriteLine($"{price:f2}");
