7b53f2480a46f0ce609f4dea1467085434be1349
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:51:04 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/06. DataTypesAndVariablesLab/05. ConvertCelsiusToFahrenheit/Program.cs
@@ -0,0 +1,5 @@
+﻿double temperaturInCelsiu = double.Parse(Console.ReadLine());
+
+double temperatureInFahrenheit = temperaturInCelsiu * 1.8 + 32;
+
+Console.WriteLine($"{temperatureInFahrenheit:f2}");
