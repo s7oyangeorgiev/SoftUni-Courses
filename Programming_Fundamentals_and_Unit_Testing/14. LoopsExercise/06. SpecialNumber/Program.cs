f9c51c3562ae95739f5920b18c89f481f738e770
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 18:22:43 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/14. LoopsExercise/06. SpecialNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int numCheck = num;
+bool isSpecial = false;
+
+while (numCheck > 0)
+{
+    int lastDigit = numCheck % 10;
+
+    if (num % lastDigit == 0)
+    {
+        isSpecial = true;
+    }
+    else
+    {
+        isSpecial = false;
+        break;
+    }
+
+    numCheck /= 10;
+}
+
+if (isSpecial)
+{
+    Console.WriteLine($"{num} is special");
+}
+else
+{
+    Console.WriteLine($"{num} is not special");
+}
