90a06db8fab3e118f116b44da7aadd8dc43d096e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:13:42 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/01. StupidPasswords/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+for (int first = 2; first <= num; first += 2)
+{
+    for (int second = 1; second <= num; second += 2)
+    {
+        int third = first * second;
+
+        Console.Write($"{first}{second}{third} ");
+    }
+}
+Console.WriteLine();
+
+
+
+
+
