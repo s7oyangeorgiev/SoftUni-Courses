533872f8838ffe97ce726b1012075ef9f50a546f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 17:59:15 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/12. ForLoopLab/06. ExamCountdown/Program.cs
@@ -0,0 +1,11 @@
+﻿int d = int.Parse(Console.ReadLine());
+
+for (int i = d; i >= 1; i--)
+{
+    Console.WriteLine($"{i} days before the exam");
+
+    if ( i == 1)
+    {
+        Console.WriteLine("The exam has come");
+    }
+}
