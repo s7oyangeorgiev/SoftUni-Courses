f5298041321957cbd70c916b88a638e7dec68679
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:28:27 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/09. ForLoop-Lab/07. SumNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int numQuantity = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 0; i < numQuantity; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    sum += num;
+}
+Console.WriteLine(sum);
