1e05eac666b793b549b256485d568e881fd89bea
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:05:39 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/06. ConditionalStatementsExercise/08. LunchBreak/Program.cs
@@ -0,0 +1,18 @@
+﻿string nameOfSerial = Console.ReadLine();
+int timeOfEpisode = int.Parse(Console.ReadLine());
+int timeForRest = int.Parse(Console.ReadLine());
+
+double timeForLunch = timeForRest / 8.00;
+double timeForRecreation = timeForRest / 4.00;
+double timeLeft = timeForRest - timeForLunch - timeForRecreation;
+
+if (timeLeft >= timeOfEpisode)
+{
+    Console.WriteLine($"You have enough time to watch {nameOfSerial} and left with {Math.Ceiling(timeLeft - timeOfEpisode)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {nameOfSerial}, you need {Math.Ceiling(timeOfEpisode - timeLeft)} more minutes.");
+}
+
+
