c84e64319d4c4beda4e07f059fabc506d6bc1f87
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:14:38 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/17. NestedLoopsAndMethodsExercise/03. UniquePinCodes/Program.cs
@@ -0,0 +1,20 @@
+﻿int max1 = int.Parse(Console.ReadLine());
+int max2 =  int.Parse(Console.ReadLine());
+int max3 =  int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= max1; i++)
+{
+    for (int j = 1; j <= max2; j++)
+    {
+        for (int k = 1; k <= max3; k++)
+        {
+            if ( i % 2 == 0 && k % 2 == 0)
+            {
+                if ( j == 2 || j == 3 || j == 5 || j == 7)
+                {
+                    Console.WriteLine($"{i}{j}{k}");
+                }
+            }
+        }
+    }
+}
