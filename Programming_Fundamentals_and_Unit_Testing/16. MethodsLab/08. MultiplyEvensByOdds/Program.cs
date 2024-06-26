35ddb567467005800e10c074f98d7d4c2115f83b
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 19:24:06 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/16. MethodsLab/08. MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,42 @@
+﻿int num = Math.Abs(int.Parse(Console.ReadLine()));
+int result = GetMultipleOfEvenAndOdds(num);
+Console.WriteLine(result);
+
+static int GetMultipleOfEvenAndOdds(int num)
+{
+    return GetSumOfEvenDigits(num) * GetSumOfOddDigits(num);
+}
+
+static int GetSumOfEvenDigits(int num)
+{
+    int evenSum = 0;
+
+    while (num > 0)
+    {
+        int digit = num % 10;
+        num /= 10;
+
+        if (digit % 2 == 0)
+        {
+            evenSum += digit;
+        }
+    }
+    return evenSum;
+}
+
+static int GetSumOfOddDigits(int num)
+{
+    int oddSum = 0;
+
+    while (num > 0)
+    {
+        int digit = num % 10;
+        num /= 10;
+
+        if (digit % 2 == 1)
+        {
+            oddSum += digit;
+        }
+    }
+    return oddSum;
+}
