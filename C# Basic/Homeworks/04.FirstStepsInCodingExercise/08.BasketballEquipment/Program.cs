bccc8803ab6eba4b7563200ee08b36ce01ef3a0d
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:55:49 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/04.FirstStepsInCodingExercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,28 @@
+﻿// incorrect code
+
+int feeForYear = int.Parse(Console.ReadLine());
+
+double percentForShoes = feeForYear * 0.4;
+double percentForClotes = percentForShoes * 0.2;
+
+double moneyForShoes = feeForYear - percentForShoes;
+double moneyForClothes = moneyForShoes - percentForClotes;
+double moneyForBall = moneyForClothes * 0.25;
+double moneyForАccessories = moneyForBall * 0.2;
+double tottalPriceForEquipment = feeForYear + moneyForShoes + moneyForClothes + moneyForBall + moneyForАccessories;
+
+Console.WriteLine(tottalPriceForEquipment);
+
+// correct code
+
+int feeForYear = int.Parse(Console.ReadLine());
+
+double moneyForShoes = feeForYear * 0.6;
+double moneyForClothes = moneyForShoes * 0.8;
+double moneyForBall = moneyForClothes * 0.25;
+double moneyForАccessories = moneyForBall * 0.2;
+double tottalPriceForEquipment = feeForYear + moneyForShoes + moneyForClothes + moneyForBall + moneyForАccessories;
+
+Console.WriteLine(tottalPriceForEquipment);
+
+
