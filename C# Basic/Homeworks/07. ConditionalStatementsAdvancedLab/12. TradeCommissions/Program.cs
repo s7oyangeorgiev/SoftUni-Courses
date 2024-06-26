848d795e65cfea1a47d2a3c7eb7b99caf121d359
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 22:18:09 +0300

    Add files via upload

---
+++ C# Basic/Homeworks/07. ConditionalStatementsAdvancedLab/12. TradeCommissions/Program.cs
@@ -0,0 +1,39 @@
+﻿string city = Console.ReadLine();
+double sales = double.Parse(Console.ReadLine());
+
+double comission = 0;
+bool isTrue = false;
+
+if (city == "Sofia")
+{
+    if (sales >= 0 && sales <= 500) {  comission = sales * 0.05; }
+    else if ( sales > 500 && sales <= 1000) { comission = sales * 0.07; }
+    else if (sales > 1_000 && sales <= 10_000) { comission = sales * 0.08; }
+    else if (sales > 10_000) { comission = sales * 0.12; }
+    else { Console.WriteLine("error"); isTrue = true; }
+}
+else if (city == "Varna")
+{
+    if (sales >= 0 && sales <= 500) { comission = sales * 0.045; }
+    else if (sales > 500 && sales <= 1000) { comission = sales * 0.075; }
+    else if (sales > 1_000 && sales <= 10_000) { comission = sales * 0.10; }
+    else if (sales > 10_000) { comission = sales * 0.13; }
+    else { Console.WriteLine("error"); isTrue = true; }
+}
+else if (city == "Plovdiv")
+{
+    if (sales >= 0 && sales <= 500) { comission = sales * 0.055; }
+    else if (sales > 500 && sales <= 1000) { comission = sales * 0.08; }
+    else if (sales > 1_000 && sales <= 10_000) { comission = sales * 0.12; }
+    else if (sales > 10_000) { comission = sales * 0.145; }
+    else { Console.WriteLine("error"); isTrue = true; }
+}
+else
+{
+    Console.WriteLine("error");
+    isTrue = true;
+}
+if (!isTrue)
+{
+    Console.WriteLine($"{comission:f2}");
+}
