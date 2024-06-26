7039c21ea41e8ab4526fa37707002c43e865afa9
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 21:26:42 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/20. ArraysAndListsExercise/04. SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿List<decimal> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(decimal.Parse)
+    .ToList();
+
+bool sumOccurred = true;
+
+while (sumOccurred)
+{
+    sumOccurred = false;
+    for (int i = 0; i < numbers.Count - 1; i++)
+    {
+        if (numbers[i] == numbers[i + 1])
+        {
+            numbers[i] += numbers[i + 1];
+            numbers.RemoveAt(i + 1);
+            sumOccurred = true;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
