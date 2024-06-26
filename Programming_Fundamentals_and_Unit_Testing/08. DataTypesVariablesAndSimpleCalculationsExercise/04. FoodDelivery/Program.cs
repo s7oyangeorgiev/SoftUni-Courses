cd7827dcf3ceae7ea28287dc2129dc4d18ad9c69
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-26 09:58:24 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/08. DataTypesVariablesAndSimpleCalculationsExercise/04. FoodDelivery/Program.cs
@@ -0,0 +1,13 @@
+﻿int chickenMenus = int.Parse(Console.ReadLine());
+int fishMenus = int.Parse(Console.ReadLine());
+int vegetarianMenus =  int.Parse(Console.ReadLine());
+
+double chickenMenusCosts = chickenMenus * 10.35;
+double fishMenusCosts = fishMenus * 12.40;
+double vegetarianMenusCosts = vegetarianMenus * 8.15;
+double totalMenuCosts = chickenMenusCosts + fishMenusCosts + vegetarianMenusCosts;
+double desertCosts = totalMenuCosts * 0.20;
+double delivery = 2.50;
+double totalCosts = totalMenuCosts + desertCosts + delivery;
+
+Console.WriteLine($"{totalCosts}");
