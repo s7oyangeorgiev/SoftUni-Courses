f9c826d5cc63628363558854fde86d886212c683
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:13:52 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/02. ConditionalStatements-MoreExercises/05. Pets/Program.cs
@@ -0,0 +1,25 @@
+﻿//input
+
+int days = int.Parse(Console.ReadLine());
+int foodLeftInKg = int.Parse(Console.ReadLine());
+double foodForDogPerDayInKg = double.Parse(Console.ReadLine());
+double foodForCatPerDayInKg = double.Parse(Console.ReadLine());
+double foodForTurtlePerDayInGrams = double.Parse(Console.ReadLine());
+
+//calculations
+double foodNeededForDog = days * foodForDogPerDayInKg;
+double foodNeededForCat = days * foodForCatPerDayInKg;
+double foodNeededForTurtle = (days * foodForTurtlePerDayInGrams) / 1000;
+double totalFoodNeeded = foodNeededForDog + foodNeededForCat + foodNeededForTurtle;
+
+//output
+if (foodLeftInKg >= totalFoodNeeded)
+{
+    double foodRemainder = foodLeftInKg - totalFoodNeeded;
+    Console.WriteLine($"{Math.Floor(foodRemainder)} kilos of food left.");
+}
+else
+{
+    double foodNeeded = totalFoodNeeded - foodLeftInKg;
+    Console.WriteLine($"{Math.Ceiling(foodNeeded)} more kilos of food are needed.");
+}
