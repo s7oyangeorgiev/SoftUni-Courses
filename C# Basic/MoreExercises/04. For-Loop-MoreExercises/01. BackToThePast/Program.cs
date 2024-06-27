e41f2e40e86b95daf73656356e5d1846904a6fd5
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:23:45 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/04. For-Loop-MoreExercises/01. BackToThePast/Program.cs
@@ -0,0 +1,30 @@
+﻿
+double inheritedMoney = double.Parse(Console.ReadLine());
+int yearToLieve = int.Parse(Console.ReadLine());
+
+int age = 18;
+double moneySpent = 0;
+int yearBack = 1800;
+
+for (int i = yearBack; i <= yearToLieve; i++)
+{
+    if (i % 2 == 0)
+    {
+        moneySpent += 12_000;
+        age++;
+    }
+    else
+    {
+        moneySpent += 12_000 + (50 * age);
+        age++;
+    }
+}
+if (inheritedMoney >= moneySpent)
+{
+    Console.WriteLine($"Yes! He will live a carefree life and will have {inheritedMoney - moneySpent:f2} dollars left.");
+}
+else
+{
+    Console.WriteLine($"He will need {moneySpent - inheritedMoney:f2} dollars to survive.");
+}
+    
