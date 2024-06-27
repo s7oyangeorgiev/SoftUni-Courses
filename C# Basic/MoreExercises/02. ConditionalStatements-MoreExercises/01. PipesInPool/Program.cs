e2825c7e19995602d9c7e9eaf6fd597599cf1530
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:12:01 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/02. ConditionalStatements-MoreExercises/01. PipesInPool/Program.cs
@@ -0,0 +1,23 @@
+﻿double vPoolVolumeInLiters = double.Parse(Console.ReadLine());
+double p1 = double.Parse(Console.ReadLine());
+double p2 = double.Parse(Console.ReadLine());
+double hHoursWorkerMissing = double.Parse(Console.ReadLine());
+
+double firstPipe = hHoursWorkerMissing * p1;
+double secondPipe = hHoursWorkerMissing * p2;
+double totalFirstAndSecondPipe = firstPipe + secondPipe;
+double fullyPoolInPecent = (totalFirstAndSecondPipe / vPoolVolumeInLiters) * 100;
+double firstPipePercent = (firstPipe / totalFirstAndSecondPipe) * 100;
+double secondPipePercent = (secondPipe / totalFirstAndSecondPipe) * 100;
+
+
+if (totalFirstAndSecondPipe <= vPoolVolumeInLiters)
+{
+    Console.WriteLine($"The pool is {fullyPoolInPecent:f2}% full. " + 
+        $"Pipe 1: {firstPipePercent:f2}%. Pipe 2: {secondPipePercent:f2}%.");
+}
+else
+{
+    Console.WriteLine($"For {hHoursWorkerMissing} " +
+        $"hours the pool overflows with {totalFirstAndSecondPipe - vPoolVolumeInLiters:f2} liters.");
+}
