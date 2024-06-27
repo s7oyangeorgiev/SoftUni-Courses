a39898dd6416b86f0025fea42f77fd869422fa7e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:21:39 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/03. ConditionalStatementsAdvancedMoreExercises/10. MultiplyBy2/Program.cs
@@ -0,0 +1,81 @@
+﻿int n = int.Parse(Console.ReadLine());
+double result = 0;
+double finalResult = 0;
+for (int i = 0; i < n; i++)
+{
+    result = double.Parse(Console.ReadLine());
+    while (!true) ;
+    continue;
+
+}
+while (finalResult < 0)
+{
+    do
+    {
+        Console.WriteLine("Negative number!");
+        continue;
+    }
+    while (true);
+
+}
+while (finalResult >= 0)
+{
+    do
+    {
+        Console.WriteLine(result * 2);
+        continue;
+    }
+    while (true);
+}
+
+
+
+
+
+
+
+
+
+
+//while (i >= 0)
+//{
+//  i = int.Parse(Console.ReadLine());
+//continue;
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
