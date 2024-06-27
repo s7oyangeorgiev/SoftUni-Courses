12f23fdd57fc53c0f539c484c76140964b653d9a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:07:53 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/05. TrainingLab/Program.cs
@@ -0,0 +1,8 @@
+﻿double lenght = double.Parse(Console.ReadLine()) * 100;
+double width  = double.Parse(Console.ReadLine()) * 100;
+
+double deskInRow = Math.Floor((width - 100) / 70);
+double rows = Math.Floor(lenght / 120);
+double totalPlaces = Math.Floor(deskInRow * rows) - 3;
+
+Console.WriteLine(totalPlaces);
