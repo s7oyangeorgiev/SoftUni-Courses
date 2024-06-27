ef285789f360046e09795a7be42cb4f827a291f0
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:24:16 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/04. For-Loop-MoreExercises/02. Hospital/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int periodForCalculations = int.Parse(Console.ReadLine());
+
+int doctors = 7;
+int unreviewedPatients = 0;
+int reviewedPatients = 0;
+int checkedDay = 0;
+
+for (int i = 0; i < periodForCalculations; i++)
+{
+    int patientsForDay = int.Parse(Console.ReadLine());
+    checkedDay += 1;
+
+    if (checkedDay % 3 == 0 && reviewedPatients < unreviewedPatients)
+    {
+        doctors += 1;
+    }
+    if (doctors <= patientsForDay)
+    {
+        unreviewedPatients += patientsForDay - doctors;
+        reviewedPatients += doctors;
+        
+    }
+    else
+    {
+        if (patientsForDay == doctors)
+        {
+            reviewedPatients += doctors;
+            
+        }
+        else if (patientsForDay < doctors)
+        {
+            reviewedPatients += patientsForDay;
+            
+        }
+    }
+}
+
+Console.WriteLine($"Treated patients: {reviewedPatients}.");
+Console.WriteLine($"Untreated patients: {unreviewedPatients}.");
