ef6a8ec238b6c8dd886ae4a6837cb3fa56c6ac9e
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:05:05 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/02. TriangleArea/Program.cs
@@ -0,0 +1,6 @@
+﻿double sideOfTriangle = double.Parse(Console.ReadLine());
+double heightOfTriangle = double.Parse(Console.ReadLine());
+
+double area = sideOfTriangle * heightOfTriangle / 2;
+
+Console.WriteLine($"{area:f2}");
