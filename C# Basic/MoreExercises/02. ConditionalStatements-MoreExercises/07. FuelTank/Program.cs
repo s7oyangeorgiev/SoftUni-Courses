d2c864d754c81ae10afc75d2a2ad0e9207c17eae
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:14:50 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/02. ConditionalStatements-MoreExercises/07. FuelTank/Program.cs
@@ -0,0 +1,19 @@
+﻿//input
+string tipeOfFuel = Console.ReadLine();
+double litresInTheTank = double.Parse(Console.ReadLine());
+
+//calculations and output
+if (litresInTheTank >= 25 && (tipeOfFuel == "Diesel" || tipeOfFuel == "Gasoline" || tipeOfFuel == "Gas"))
+{
+    string lCase = tipeOfFuel.ToLower();
+    Console.WriteLine($"You have enough {lCase}.");
+}
+else if (litresInTheTank < 25 && (tipeOfFuel == "Diesel" || tipeOfFuel == "Gasoline" || tipeOfFuel == "Gas"))
+{
+    string lCase = tipeOfFuel.ToLower();
+    Console.WriteLine($"Fill your tank with {lCase}!");
+}
+else 
+{
+    Console.WriteLine("Invalid fuel!");
+}
