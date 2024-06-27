f72077187e4905febd7e73a1daeb42836dd0c78f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:04:33 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/01. TrapeziodArea/Program.cs
@@ -0,0 +1,7 @@
+﻿double sideB1 = double.Parse(Console.ReadLine());
+double sideB2 = double.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
+
+double area = (sideB1 + sideB2) * h / 2;
+
+Console.WriteLine($"{area:f2}");
