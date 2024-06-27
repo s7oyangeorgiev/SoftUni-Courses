23a380a9f1bc756191b4713014c456a3eac4e2cf
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:31:12 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/05. While-Loop-MoreExercises/03. StreamOfLetters/Program.cs
@@ -0,0 +1,65 @@
+﻿
+string symbol = Console.ReadLine();
+
+int secretCharCounterC = 0;
+int secretCharCounterO = 0;
+int secretCharCounterN = 0;
+string word = "";
+string allOutput = "";
+
+
+while (symbol != "End")
+{
+    char currentSymbol = char.Parse(symbol);
+    
+
+    if ((currentSymbol >= 'a' && currentSymbol <= 'z') || (currentSymbol >= 'A' && currentSymbol <= 'Z'))
+    {
+        if (currentSymbol == 'c')
+        {
+            secretCharCounterC++;
+
+            if (secretCharCounterC > 1)
+            {
+                secretCharCounterC -= 1;
+                word += currentSymbol;
+            }
+        }
+        else if (currentSymbol == 'o')
+        {
+            secretCharCounterO++;
+
+            if (secretCharCounterO > 1)
+            {
+                secretCharCounterO -= 1;
+                word += currentSymbol;
+            }
+        }
+        else if (currentSymbol == 'n')
+        {
+            secretCharCounterN++;
+
+            if (secretCharCounterN > 1)
+            {
+                secretCharCounterN -= 1;
+                word += currentSymbol;
+            }
+        }
+        else
+        {
+            word += currentSymbol;
+        }
+
+        if ((secretCharCounterC + secretCharCounterO + secretCharCounterN) == 3)
+        {
+            secretCharCounterC = 0;
+            secretCharCounterO = 0;
+            secretCharCounterN = 0;
+            word += " ";
+            allOutput = word;
+        }
+    }
+
+    symbol = Console.ReadLine();
+}
+Console.WriteLine(allOutput);
