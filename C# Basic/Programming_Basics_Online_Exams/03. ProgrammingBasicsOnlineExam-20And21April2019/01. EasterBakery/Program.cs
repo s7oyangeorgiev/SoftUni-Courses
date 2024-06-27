851e6785a54072c8ff5174142970d52494883dcb
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 11:07:59 +0300

    Add files via upload

---
+++ C# Basic/Programming_Basics_Online_Exams/03. ProgrammingBasicsOnlineExam-20And21April2019/01. EasterBakery/Program.cs
@@ -0,0 +1,19 @@
+﻿
+double priceForKgFlour = double.Parse(Console.ReadLine());
+double flourKg = double.Parse(Console.ReadLine());
+double sugarKg = double.Parse(Console.ReadLine());
+int eggsX12 = int.Parse(Console.ReadLine());
+int packsOfYeast =  int.Parse(Console.ReadLine());
+
+double priceForKgSugar = priceForKgFlour * 0.75;
+double priceForEggsX12 = priceForKgFlour * 1.10;
+double priceForYeast = priceForKgSugar * 0.20;
+
+double tottalPriceForSugar = priceForKgSugar * sugarKg;
+double tottalPriceFOrEggs = eggsX12 * priceForEggsX12;
+double tottalPriceForYeast = packsOfYeast * priceForYeast;
+double tottalPriceForFlour = flourKg * priceForKgFlour;
+
+double tottalPrice = tottalPriceForSugar + tottalPriceFOrEggs + tottalPriceForYeast + tottalPriceForFlour;
+
+Console.WriteLine($"{tottalPrice:f2}");
