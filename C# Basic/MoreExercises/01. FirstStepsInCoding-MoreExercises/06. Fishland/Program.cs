79c56f8a744b961e37be53f34b7b803ee6a905a3
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:08:23 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/06. Fishland/Program.cs
@@ -0,0 +1,13 @@
+﻿double scumriyaBgnPriceForKg = double.Parse(Console.ReadLine());
+double cacaBgnPriceForKg = double.Parse(Console.ReadLine());
+double palamudQuantity = double.Parse(Console.ReadLine());
+double safridQuantity = double.Parse(Console.ReadLine());
+double midiQuantity = double.Parse(Console.ReadLine());
+
+double palamudPrice = palamudQuantity * (scumriyaBgnPriceForKg * 1.60);
+double safridPrice = safridQuantity * (cacaBgnPriceForKg * 1.80);
+double midiPrice = midiQuantity * 7.50;
+
+double totalPrice = palamudPrice + safridPrice + midiPrice;
+
+Console.WriteLine($"{totalPrice:f2}");
