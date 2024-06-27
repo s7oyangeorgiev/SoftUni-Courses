30e7b1005b4ab5ef43cd4a60ef2d9218a57ad8cd
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:13:24 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/02. ConditionalStatements-MoreExercises/04. TransportPrice/Program.cs
@@ -0,0 +1,25 @@
+﻿//input
+int nKm = int.Parse(Console.ReadLine());
+string word = Console.ReadLine();
+
+//calculations + output
+if (nKm < 20 && word == "day")
+{
+    double taxiCoast = 0.70 + nKm * 0.79;
+    Console.WriteLine($"{taxiCoast:f2}");
+}
+else if (nKm < 20 && word == "night")
+{
+    double taxiCoast = 0.70 + nKm * 0.90;
+    Console.WriteLine($"{taxiCoast:f2}");
+}
+else if (nKm >= 20 && nKm <100)
+{
+    double busCoast = nKm * 0.09;
+    Console.WriteLine($"{busCoast:f2}");
+}
+else
+{
+    double trainCoast = nKm * 0.06;
+    Console.WriteLine($"{trainCoast:f2}");
+}
