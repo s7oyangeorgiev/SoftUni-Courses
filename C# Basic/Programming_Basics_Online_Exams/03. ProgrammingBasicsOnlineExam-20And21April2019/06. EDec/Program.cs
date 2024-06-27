81d11ef09225004da219efc064c58fb59065c787
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 11:12:42 +0300

    Add files via upload

---
+++ C# Basic/Programming_Basics_Online_Exams/03. ProgrammingBasicsOnlineExam-20And21April2019/06. EDec/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Transactions;
+
+int customerCount = int.Parse(Console.ReadLine());
+
+double price = 0;
+int purchaseCount = 0;
+double tottalPrice = 0;
+
+
+for (int i = 0; i < customerCount; i++)
+{
+    while (true)
+    {
+        string purchase = Console.ReadLine();
+        
+        if (purchase == "Finish")
+        {
+            if (purchaseCount % 2 == 0)
+            {
+                price *= 0.80;
+            }
+
+            Console.WriteLine($"You purchased {purchaseCount} items for {price:f2} leva.");
+            tottalPrice += price;
+            price = 0;
+            purchaseCount = 0;
+            break;
+        }
+
+        purchaseCount++;
+
+        switch (purchase)
+        {
+            case "basket": price += 1.50; break;
+            case "wreath": price += 3.80; break;
+            case "chocolate bunny": price += 7.00; break;
+        }
+
+        
+    }
+}
+double averageBill = tottalPrice / customerCount;
+
+Console.WriteLine($"Average bill per client is: {averageBill:f2} leva.");
---
+++ C# Basic/Programming_Basics_Online_Exams/03. ProgrammingBasicsOnlineExam-20And21April2019/06. EstrCom/Program.cs
@@ -0,0 +1,45 @@
+﻿
+int easterBreadCount = int.Parse(Console.ReadLine());
+
+int tottalPoints = 0;
+string bestCoocker = "";
+int maxPoints = int.MinValue;
+int coockerCount = 0;
+
+for (int i = 0; i < easterBreadCount; i++)
+{
+    string nameOfCoocker = Console.ReadLine();
+    coockerCount++;
+
+    while (nameOfCoocker != "Stop")
+    {
+        string grade = Console.ReadLine();
+
+        while (grade != "Stop")
+        {
+            int currentGrade = int.Parse(grade);
+
+            tottalPoints += currentGrade;
+
+            grade = Console.ReadLine();
+        }
+        if (grade == "Stop")
+        {
+            Console.WriteLine($"{nameOfCoocker} has {tottalPoints} points.");
+
+            if (tottalPoints > maxPoints)
+            {
+                maxPoints = tottalPoints;
+                bestCoocker = nameOfCoocker;
+                Console.WriteLine($"{bestCoocker} is the new number 1!");
+            }
+            tottalPoints = 0;
+            break;
+        }
+    }
+    if (coockerCount == easterBreadCount)
+    {
+        Console.WriteLine($"{bestCoocker} won competition with {maxPoints} points!");
+        break;
+    }
+}
