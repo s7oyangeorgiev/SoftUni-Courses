b49c4cd36810be0f476cdb305e0aa13d02862b2d
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:09:23 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/08. CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,7 @@
+﻿double r = double.Parse(Console.ReadLine());
+
+double area = Math.PI * r * r;
+double perimeter = 2 * Math.PI * r;
+
+Console.WriteLine($"{area:f2}");
+Console.WriteLine($"{perimeter:f2}");
