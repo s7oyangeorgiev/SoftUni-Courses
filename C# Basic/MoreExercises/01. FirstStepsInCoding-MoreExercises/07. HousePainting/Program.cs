7aa22cc0b6d8f193c2d944cc6f64738ed24a3b0f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:08:55 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/07. HousePainting/Program.cs
@@ -0,0 +1,25 @@
+﻿//input
+double xHeightOfTheHouse = double.Parse(Console.ReadLine());
+double ySidewallLength = double.Parse(Console.ReadLine());
+double hTriangularWallOfTheEaves = double.Parse(Console.ReadLine());
+
+//calculation
+//walls
+double sideWallArea = xHeightOfTheHouse * ySidewallLength;
+double windowArea = 1.5 * 1.5;
+double bothSides = (2 * sideWallArea) - (2 * windowArea);
+double backWall = xHeightOfTheHouse * xHeightOfTheHouse;
+double entryDoor = 1.2 * 2;
+double totalBackAndEntry = (2 * backWall) - entryDoor;
+double totalAreaWalls = bothSides + totalBackAndEntry;
+double greenPaint = totalAreaWalls / 3.4;
+//eaves
+double bothRectangleOfTheEaves = 2 * (xHeightOfTheHouse * ySidewallLength);
+double bothTriangleOfTheEaves = 2 * (xHeightOfTheHouse * hTriangularWallOfTheEaves / 2);
+double totalAreaEaves = bothRectangleOfTheEaves + bothTriangleOfTheEaves;
+double redPaint = totalAreaEaves / 4.3;
+
+//output
+Console.WriteLine($"{greenPaint:f2}");
+Console.WriteLine($"{redPaint:f2}");
+    
