a5a0448532ac01b3d2ecc5cbb99a76348d30426a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:30:47 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/05. While-Loop-MoreExercises/02. ReportSystem/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+int sumTarget = int.Parse(Console.ReadLine());
+string price = Console.ReadLine();
+
+double tottalMoney = 0;
+double tottalCashMoney = 0;
+double tottalCardMoney = 0;
+int paymentCashCounter = 0;
+int paymentCardCounter = 0;
+int paymentCounter = 0;
+string paymentMetod = "";
+
+while (price != "End")
+{
+    double currentPrice = double.Parse(price);
+    paymentCounter++;
+
+    if (paymentCounter % 2 == 1)
+    {
+        paymentMetod = "cash";
+
+        if (currentPrice > 100)
+        {
+            Console.WriteLine("Error in transaction!");
+            tottalMoney += 0;
+        }
+        else
+        {
+            Console.WriteLine("Product sold!");
+            tottalMoney += currentPrice;
+            tottalCashMoney += currentPrice;
+            paymentCashCounter++;
+        }
+
+    }
+    else
+    {
+        paymentMetod = "card";
+
+        if (currentPrice < 10)
+        {
+            Console.WriteLine("Error in transaction!");
+            tottalMoney += 0;
+        }
+        else
+        {
+            Console.WriteLine("Product sold!");
+            tottalMoney += currentPrice;
+            tottalCardMoney += currentPrice;
+            paymentCardCounter++;
+        }
+    }
+
+    if (tottalMoney >= sumTarget)
+    {
+        break;
+    }
+
+    price = Console.ReadLine();
+}
+double avarageWhitCash = tottalCashMoney / paymentCashCounter;
+double avarageWhitCard = tottalCardMoney / paymentCardCounter;
+
+if (tottalMoney >= sumTarget)
+{
+    Console.WriteLine($"Average CS: {avarageWhitCash:f2}");
+    Console.WriteLine($"Average CC: {avarageWhitCard:f2}");
+}
+else
+{
+    Console.WriteLine("Failed to collect required money for charity.");
+}
+
