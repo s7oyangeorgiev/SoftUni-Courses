e79af254884235b4a4d7993b3510fa42e9e8ef49
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:26:33 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/04. For-Loop-MoreExercises/06. Bills/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int mounth = int.Parse(Console.ReadLine());
+
+double totalSumFOrElectricity = 0;
+double sumForWater = 0;
+double sumForInternet = 0;
+double sumForOthers = 0;
+for (int i = 0; i < mounth; i++)
+{
+    double recieveForElectricity = double.Parse(Console.ReadLine());
+    totalSumFOrElectricity += recieveForElectricity;
+    sumForWater += 20;
+    sumForInternet += 15;
+    sumForOthers = (totalSumFOrElectricity + sumForWater + sumForInternet) * 120 / 100;
+}
+double middleForMounth = (totalSumFOrElectricity + sumForWater + sumForInternet + sumForOthers) / mounth;
+
+Console.WriteLine($"Electricity: {totalSumFOrElectricity:f2} lv");
+Console.WriteLine($"Water: {sumForWater:f2} lv");
+Console.WriteLine($"Internet: {sumForInternet:f2} lv");
+Console.WriteLine($"Other: {sumForOthers:f2} lv");
+Console.WriteLine($"Average: {middleForMounth:f2} lv");
