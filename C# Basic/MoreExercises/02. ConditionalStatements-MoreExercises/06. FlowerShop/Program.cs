2a5c8bf45da99b6cbe6f6bd3d8e7e0910edf6d81
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:14:23 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/02. ConditionalStatements-MoreExercises/06. FlowerShop/Program.cs
@@ -0,0 +1,29 @@
+﻿//input
+int numbersOfMagnolias = int.Parse(Console.ReadLine());
+int numbersOfZiumbiuli = int.Parse(Console.ReadLine());
+int numbersOfRoses = int.Parse(Console.ReadLine());
+int numbersOfCactus = int.Parse(Console.ReadLine());
+double priceOfPresent = double.Parse(Console.ReadLine());
+
+double priceOfMagnolias = 3.25;
+double priceOfZiumbiuli = 4.00;
+double priceOfRoses = 3.50;
+double priceOfCactus = 8.00;
+
+//calculations
+double price = (numbersOfMagnolias * priceOfMagnolias) + (numbersOfZiumbiuli * priceOfZiumbiuli) +
+    (numbersOfRoses * priceOfRoses) + (numbersOfCactus * priceOfCactus);
+double tax = price * 0.05;
+double profit = price - tax;
+
+//output
+if (priceOfPresent > profit)
+{
+    double lack = priceOfPresent - profit;
+    Console.WriteLine($"She will have to borrow {Math.Ceiling(lack)} leva.");
+}
+else
+{
+    double lack = profit - priceOfPresent;
+    Console.WriteLine($"She is left with {Math.Floor(lack)} leva.");
+}
