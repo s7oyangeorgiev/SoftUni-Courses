873640fa61994156e060a3c0ad0021028fe1507d
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:27:33 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/04. For-Loop-MoreExercises/08. EqualPairs/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int num = int.Parse(Console.ReadLine());
+
+int sumOne = 0;
+int sumTwo = 0;
+int diff = 0;
+
+for (int i = 0; i < num; i++)
+{
+    int currentNumOne = int.Parse(Console.ReadLine());
+    int currentNumTwo = int.Parse(Console.ReadLine());
+
+    int sumOfNumbers = currentNumOne + currentNumTwo;
+
+    if (i > 0)
+    {
+        int diffBetweenCycles = Math.Abs(sumOfNumbers - (sumOne + sumTwo));
+
+        if (diffBetweenCycles > diff)
+        {
+            diff = diffBetweenCycles;
+        }
+    }
+    sumOne = currentNumOne;
+    sumTwo = currentNumTwo;
+}
+if (diff == 0)
+{
+    Console.WriteLine($"Yes, value={sumOne + sumTwo}");
+}
+else
+{
+    Console.WriteLine($"No, maxdiff={diff}");
+}
