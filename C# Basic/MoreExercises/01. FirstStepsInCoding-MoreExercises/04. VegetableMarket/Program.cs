6b03d7aa7f525889ef296d8f11ffbc86b0afce84
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 10:06:07 +0300

    Add files via upload

---
+++ C# Basic/MoreExercises/01. FirstStepsInCoding-MoreExercises/04. VegetableMarket/Program.cs
@@ -0,0 +1,11 @@
+﻿double priceForOneKgVegetables = double.Parse(Console.ReadLine());
+double priceForOneKgFruits =  double.Parse(Console.ReadLine());
+int totalKgOfVegetables = int.Parse(Console.ReadLine());
+int totalKgOfFruits =  int.Parse(Console.ReadLine());
+
+double priceOfVegetables = totalKgOfVegetables * priceForOneKgVegetables;
+double priceOfFruits = totalKgOfFruits * priceForOneKgFruits;
+
+double total = (priceOfVegetables + priceOfFruits) / 1.94;
+
+Console.WriteLine($"{total:f2}");
