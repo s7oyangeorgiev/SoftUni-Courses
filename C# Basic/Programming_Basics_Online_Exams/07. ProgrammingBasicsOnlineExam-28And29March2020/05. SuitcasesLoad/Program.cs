5d678b04b8fa69352b2e03033745255f2492ade8
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-27 11:39:54 +0300

    Add files via upload

---
+++ C# Basic/Programming_Basics_Online_Exams/07. ProgrammingBasicsOnlineExam-28And29March2020/05. SuitcasesLoad/Program.cs
@@ -0,0 +1,38 @@
+﻿
+double bagCapacity = double.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+
+int suitcaseCounter = 0;
+bool isFull = false;
+
+while (command != "End")
+{
+    double suitcaseVolume = double.Parse(command);
+    suitcaseCounter++;
+
+    if (suitcaseCounter % 3 == 0)
+    {
+        suitcaseVolume = suitcaseVolume + (suitcaseVolume * 0.10);
+    }
+    if (bagCapacity < suitcaseVolume)
+    {
+        suitcaseCounter--;
+        isFull = true;
+        break;
+    }
+
+    bagCapacity -= suitcaseVolume;
+    
+    command = Console.ReadLine();
+}
+if (command == "End")
+{
+    Console.WriteLine("Congratulations! All suitcases are loaded!");
+    Console.WriteLine($"Statistic: {suitcaseCounter} suitcases loaded.");
+}
+else if (isFull)
+{
+    Console.WriteLine("No more space!");
+    Console.WriteLine($"Statistic: {suitcaseCounter} suitcases loaded.");
+}
+
---
+++ C# Basic/Programming_Basics_Online_Exams/07. ProgrammingBasicsOnlineExam-28And29March2020/06. TournamentOfChristmas/Program.cs
@@ -0,0 +1,54 @@
+﻿
+int daysCount = int.Parse(Console.ReadLine());
+
+double profitForCharity = 0;
+double tottalProfit = 0;
+int winsCounter = 0;
+int lostCounter = 0;
+int winsDaysCounter = 0;
+int lostDaysCounter = 0;
+
+for (int i = 0; i < daysCount; i++)
+{
+    string nameOfGames = Console.ReadLine();
+    profitForCharity = 0;
+
+    while (nameOfGames != "Finish")
+    {
+        string result = Console.ReadLine();
+
+        if (result == "win")
+        {
+            profitForCharity += 20;
+            winsCounter++;
+        }
+        else
+        {
+            lostCounter++;
+        }
+        nameOfGames = Console.ReadLine();
+    }
+
+    if (winsCounter > lostCounter)
+    {
+        profitForCharity *= 1.10;
+        tottalProfit += profitForCharity;
+        winsDaysCounter++;
+    }
+    else
+    {
+        tottalProfit += profitForCharity;
+        lostDaysCounter++;
+    }
+    winsCounter = 0;
+    lostCounter = 0;
+}
+if (winsDaysCounter > lostDaysCounter)
+{
+    tottalProfit *= 1.20;
+    Console.WriteLine($"You won the tournament! Total raised money: {tottalProfit:f2}");
+}
+else
+{
+    Console.WriteLine($"You lost the tournament! Total raised money: {tottalProfit:f2}");
+}
