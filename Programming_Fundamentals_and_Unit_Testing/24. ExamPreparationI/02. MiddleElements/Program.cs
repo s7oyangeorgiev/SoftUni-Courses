84d91d695333c3ca2a86b283298024d3afba96a0
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-30 17:54:58 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/24. ExamPreparationI/02. MiddleElements/Program.cs
@@ -0,0 +1,14 @@
+﻿string numArray = Console.ReadLine();
+
+int[] numbers = numArray.Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+int middleIndex = numbers.Length / 2;
+
+int middleElement1 = numbers[middleIndex - 1];
+int middleElement2 = numbers[middleIndex];
+
+double average = (middleElement1 + middleElement2) / 2.0;
+
+Console.WriteLine($"{average:F2}");
