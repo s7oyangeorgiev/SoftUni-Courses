b4a3eaeea6b9accdba91464592b75f641069315a
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-30 19:27:45 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/26. ExamPreparationIII/01. English Name of Each Digit/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+string numString = number.ToString();
+
+string[] digitNames = {
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+        };
+
+for (int i = numString.Length - 1; i >= 0; i--)
+{
+    int digit = int.Parse(numString[i].ToString());
+    Console.WriteLine(digitNames[digit]);
+}
---
+++ Programming_Fundamentals_and_Unit_Testing/26. ExamPreparationIII/02. Average Last Elements/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+int N = int.Parse(Console.ReadLine());
+
+int startIndex = Math.Max(0, array.Length - N);
+int[] lastNElements = array.Skip(startIndex).ToArray();
+
+double average = lastNElements.Average();
+
+Console.WriteLine($"{average:F2}");
