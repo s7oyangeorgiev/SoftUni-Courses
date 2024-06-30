5226313c57c418c98c1f9170b74cc99e97f153d0
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-30 17:54:22 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/24. ExamPreparationI/01. SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿string num = Console.ReadLine();
+
+int sumOfFactorials = 0;
+
+foreach (char chars in num)
+{
+    int digit = chars - '0';
+
+    if (digit % 2 == 0)
+    {
+        int factorial = Factorial(digit);
+
+        sumOfFactorials += factorial;
+    }
+}
+
+Console.WriteLine(sumOfFactorials);
+
+static int Factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+
+    int result = 1;
+
+    for (int i = n; i > 1; i--)
+    {
+        result *= i;
+    }
+    return result;
+}
+
