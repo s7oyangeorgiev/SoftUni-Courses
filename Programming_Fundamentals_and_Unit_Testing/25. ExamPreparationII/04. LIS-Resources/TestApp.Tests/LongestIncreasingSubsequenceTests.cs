15cb59290e89cd2de61942aa2d9d46409e5f6e22
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-30 19:25:29 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/25. ExamPreparationII/04. LIS-Resources/TestApp.Tests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class LongestIncreasingSubsequenceTests
+{
+    [Test]
+    public void Test_GetLis_NullArray_ThrowsArgumentNullException()
+    {
+        // Arrange
+        int[] arr = null;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => LongestIncreasingSubsequence.GetLis(arr));
+    }
+
+    [Test]
+    public void Test_GetLis_EmptyArray_ReturnsEmptyString()
+    {
+        // Arrange
+        int[] arr = new int[] { };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(arr);
+
+        // Assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [Test]
+    public void Test_GetLis_SingleElementArray_ReturnsElement()
+    {
+        // Arrange
+        int[] arr = new int[] { 5 };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(arr);
+
+        // Assert
+        Assert.AreEqual("5", result);
+    }
+
+    [Test]
+    public void Test_GetLis_UnsortedArray_ReturnsLongestIncreasingSubsequence()
+    {
+        // Arrange
+        int[] arr = new int[] { 3, 10, 2, 1, 20 };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(arr);
+
+        // Assert
+        Assert.AreEqual("3 10 20", result);
+    }
+
+    [Test]
+    public void Test_GetLis_SortedArray_ReturnsItself()
+    {
+        // Arrange
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(arr);
+
+        // Assert
+        Assert.AreEqual("1 2 3 4 5", result);
+    }
+}
