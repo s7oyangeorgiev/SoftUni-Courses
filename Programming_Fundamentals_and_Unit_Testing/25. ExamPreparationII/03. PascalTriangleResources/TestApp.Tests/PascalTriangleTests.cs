864f732f459588a033c4354c64fe8ab29625fa85
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-30 19:21:35 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/25. ExamPreparationII/03. PascalTriangleResources/TestApp.Tests/PascalTriangleTests.cs
@@ -0,0 +1,22 @@
+using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class PascalTriangleTests
+{
+    [TestCase(1, "1 \n")]
+    [TestCase(2, "1 \n1 1 \n")]
+    [TestCase(3, "1 \n1 1 \n1 2 1 \n")]
+    [TestCase(5, "1 \n1 1 \n1 2 1 \n1 3 3 1 \n1 4 6 4 1 \n")]
+    [TestCase(6, "1 \n1 1 \n1 2 1 \n1 3 3 1 \n1 4 6 4 1 \n1 5 10 10 5 1 \n")]
+
+    public void Test_PrintTriangle_ShouldReturnCorrectString(int n, string expected)
+    {
+        // Act
+        string result = PascalTriangle.PrintTriangle(n);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
