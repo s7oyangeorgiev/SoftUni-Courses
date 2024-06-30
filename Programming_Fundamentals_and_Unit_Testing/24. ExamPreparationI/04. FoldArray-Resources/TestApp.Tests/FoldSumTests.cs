7472e97e781505e1dcb0f28d683f8a0e42876890
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-06-30 18:02:19 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/24. ExamPreparationI/04. FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class FoldSumTests
+{
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new int[] { -1, -2, -3, -4, -5, -6, -7, -8 }, "-5 -5 -13 -13")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, "5 5 14 14")]
+    [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, "0 0 0 0")]
+    [TestCase(new int[] { 1, 2, 0, 0, 0, 0, 7, 8 }, "2 1 8 7")]
+    public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
+    {
+        // Act
+            string result = FoldSum.FoldArray(arr);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+    }
+}
