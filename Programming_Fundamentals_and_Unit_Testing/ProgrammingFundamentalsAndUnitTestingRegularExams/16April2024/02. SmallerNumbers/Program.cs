5fa65292fd23eb55687f5ea29a6f18cabeae3e1c
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:15:47 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/16April2024/02. SmallerNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numArray = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+int currentNum = int.Parse(Console.ReadLine());
+
+int[] smallerNumbers = numArray.Where(num => num < currentNum).ToArray();
+
+Console.WriteLine(string.Join(" ", smallerNumbers));
