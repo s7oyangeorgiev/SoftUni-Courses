c0919b6ed49a493a62d989217b12129038b1cccc
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:16:55 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/16April2024/03. PrimeNumbers/TestApp/PrimeNumbers.cs
@@ -0,0 +1,43 @@
+﻿namespace TestApp;
+public class PrimeNumbers
+{
+    public static string GetPrimeNumbersInRange(int startNum, int endNum)
+    {
+        if (startNum > endNum)
+        {
+            return "Start number should be bigger than end number.";
+        }
+
+        string result = string.Empty;
+        for (int i = startNum; i <= endNum; i++)
+        {
+            bool isPrime = IsPrimeNumber(i);
+
+            if (isPrime)
+            {
+                result += $"{i} ";
+            }
+        }
+
+        return result.Trim();
+    }
+
+    private static bool IsPrimeNumber(int num)
+    {
+        if (num == 0 || num == 1)
+        {
+            return false;
+        }
+
+        for (int j = 2; j <= Math.Sqrt(num); j++)
+        {
+            if (num % j == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
