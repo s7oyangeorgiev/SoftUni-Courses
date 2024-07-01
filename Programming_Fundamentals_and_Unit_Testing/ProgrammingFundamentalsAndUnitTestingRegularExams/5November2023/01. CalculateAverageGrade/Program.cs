663a576ad38a02f59973ad761d733912229694ef
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:30:20 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/5November2023/01. CalculateAverageGrade/Program.cs
@@ -0,0 +1,14 @@
+﻿int countOfStudents = int.Parse(Console.ReadLine());
+
+double[] grades = new double[countOfStudents];
+double sum = 0;
+
+for (int i = 0; i < countOfStudents; i++)
+{
+    grades[i] = double.Parse(Console.ReadLine());
+    sum += grades[i];
+}
+
+double average = sum / countOfStudents;
+
+Console.WriteLine($"{average:F2}");
---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/5November2023/02. Rotations/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] numArray = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    int lastElement = numArray[numArray.Length - 1];
+
+    for (int j = numArray.Length - 1; j > 0; j--)
+    {
+        numArray[j] = numArray[j - 1];
+    }
+    numArray[0] = lastElement;
+}
+
+Console.WriteLine(string.Join(", ", numArray));
