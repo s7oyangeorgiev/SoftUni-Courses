7806ac9a6682628cc7110d27c421dd0ce685110f
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:27:43 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/20December2023/04. Messaging/TestApp/Messaging.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestApp;
+
+public class Messaging
+{
+    public static string GetMessage(List<int>? nums, string? s)
+    {
+        if (nums is null || s is null || nums.Count == 0 || s.Length == 0)
+        {
+            return string.Empty;
+        }
+
+        string result = string.Empty;
+        foreach (int n in nums)
+        {
+            int sum = n.ToString().Sum(c => c - '0');
+            if (sum >= s.Length)
+            {
+                sum %= s.Length;
+            }
+
+            result += s[sum];
+            s = s.Remove(sum, 1);
+        }
+
+        return result;
+    }
+}
