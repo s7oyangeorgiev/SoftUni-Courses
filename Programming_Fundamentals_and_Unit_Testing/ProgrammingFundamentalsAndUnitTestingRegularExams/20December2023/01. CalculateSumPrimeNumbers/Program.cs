cb57fc4ba0ade98b0b63849f823be83113575ab2
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:24:02 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/20December2023/01. CalculateSumPrimeNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿int numberCount = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[numberCount];
+
+for (int i = 0; i < numberCount; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+int sumOfPrimes = 0;
+string primeNumbersList = "";
+
+for (int i = 0; i < numberCount; i++)
+{
+    if (IsPrime(numbers[i]))
+    {
+        sumOfPrimes += numbers[i];
+        primeNumbersList += numbers[i] + " ";
+    }
+}
+
+Console.WriteLine(sumOfPrimes);
+ 
+static bool IsPrime(int number)
+{
+    if (number <= 1)
+        return false;
+    if (number == 2)
+        return true;
+    if (number % 2 == 0)
+        return false;
+
+    int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+    {
+        if (number % i == 0)
+            return false;
+    }
+
+    return true;
+}
---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/20December2023/02. BiggerNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numArray = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+int nums = int.Parse(Console.ReadLine());
+
+bool foundAny = false;
+
+foreach (int num in numArray)
+{
+    if (num > nums)
+    {
+        if (foundAny)
+        {
+            Console.Write($" {num}");
+        }
+        else
+        {
+            Console.Write($"{num}");
+            foundAny = true;
+        }
+    }
+}
+
+Console.WriteLine();
