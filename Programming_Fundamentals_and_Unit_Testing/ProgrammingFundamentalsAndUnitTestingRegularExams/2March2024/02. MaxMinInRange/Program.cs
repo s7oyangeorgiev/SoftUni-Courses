94f8f1ebead682fc58e18e5bd53b0f9cfeab4a24
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:04:40 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/2March2024/02. MaxMinInRange/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int startIndex = int.Parse(Console.ReadLine());
+
+int endIndex = int.Parse(Console.ReadLine());
+
+int minNumber = int.MaxValue;
+int maxNumber = int.MinValue;
+
+for (int i = startIndex; i <= endIndex; i++)
+{
+    if (numbers[i] < minNumber)
+    {
+        minNumber = numbers[i];
+    }
+    if (numbers[i] > maxNumber)
+    {
+        maxNumber = numbers[i];
+    }
+}
+
+int sum = maxNumber + minNumber;
+
+Console.WriteLine(sum);
