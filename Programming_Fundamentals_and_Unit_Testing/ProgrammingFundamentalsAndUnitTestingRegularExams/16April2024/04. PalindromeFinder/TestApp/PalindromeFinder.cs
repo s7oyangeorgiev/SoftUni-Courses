9717a81a9361e8d91b4016b8c1d6897911c640b8
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-01 12:19:49 +0300

    Add files via upload

---
+++ Programming_Fundamentals_and_Unit_Testing/ProgrammingFundamentalsAndUnitTestingRegularExams/16April2024/04. PalindromeFinder/TestApp/PalindromeFinder.cs
@@ -0,0 +1,35 @@
+﻿namespace TestApp;
+public class PalindromeFinder
+{
+    public static string GetPalindromes(List<string>? words)
+    {
+        if (words == null || words.Count == 0)
+        {
+            return string.Empty;
+        }
+
+        string result = string.Empty;
+        for (int i = 0; i < words.Count; i++)
+        {
+            string word = words[i].ToLower();
+            bool isPalindrome = true;
+
+            for (int j = 0; j < word.Length / 2; j++)
+            {
+                if (word[j] != word[word.Length - 1 - j])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                result += $"{words[i]} ";
+            }
+        }
+
+        return result.Trim();
+    }
+}
+
