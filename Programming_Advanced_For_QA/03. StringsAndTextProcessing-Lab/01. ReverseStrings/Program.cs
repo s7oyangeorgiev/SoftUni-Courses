9a724ba3733d6eeefd4412fced8892d88141c8d7
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-18 10:46:50 +0300

    Add files via upload

---
+++ Programming_Advanced_For_QA/03. StringsAndTextProcessing-Lab/01. ReverseStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿while (true)
+{
+    string stringSeries = Console.ReadLine();
+
+    if (stringSeries == "end")
+    {
+        break;
+    }
+
+    string reversedText = ReverseString(stringSeries);
+
+    Console.WriteLine($"{stringSeries} = {reversedText}");
+}
+
+static string ReverseString(string str)
+{
+    char[] charArray = str.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
