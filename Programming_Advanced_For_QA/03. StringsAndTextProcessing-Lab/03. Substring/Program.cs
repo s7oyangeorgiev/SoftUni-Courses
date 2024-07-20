34dd56a725d5174d62b009df474dd9c0c51bdb60
s7oyangeorgiev <160408644+s7oyangeorgiev@users.noreply.github.com>
2024-07-20 21:31:27 +0300

    Add files via upload

---
+++ Programming_Advanced_For_QA/03. StringsAndTextProcessing-Lab/03. Substring/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string firstString = Console.ReadLine();
+
+string secondString = Console.ReadLine();
+
+while (secondString.Contains(firstString))
+{
+    secondString = secondString.Replace(firstString, string.Empty);
+}
+
+Console.WriteLine(secondString);
---
+++ Programming_Advanced_For_QA/03. StringsAndTextProcessing-Lab/04. TextFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string[] bannedWords = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+
+
+foreach (string bannedWord in bannedWords)
+{
+    string replacement = new string('*', bannedWord.Length);
+    text = text.Replace(bannedWord, replacement);
+}
+
+Console.WriteLine(text);
